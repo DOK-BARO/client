610e9a1cd95e434c44b0386bbd21fae0444ca3e0
page1597 <62283847+page1597@users.noreply.github.com>
2024-06-16 15:36:55 +0900

    Init: Initial setting

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,17 +2,25 @@ module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react-hooks/recommended",
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  ignorePatterns: ["dist", ".eslintrc.cjs"],
+  parser: "@typescript-eslint/parser",
+  plugins: ["react-refresh"],
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
+    "react-refresh/only-export-components": [
+      "warn",
       { allowConstantExport: true },
     ],
+    semi: true,
+    singleQuote: false,
+    tabWidht: 2,
+    printWidth: 80,
+    bracketSpacing: true,
+    trailingComma: "all",
+    arrowParens: "always",
+    jsxSingleQuote: false,
   },
-}
+};

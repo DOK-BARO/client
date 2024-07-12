7b7a088a7d454f1dc60ea8f878eec084108cd4a1
page1597 <gimm08377@gmail.com>
2024-07-13 01:44:07 +0900

    Design(DK-66): 인풋 스타일링 적용

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,14 +1,23 @@
-import Button from "../../components/atom/button";
+// import Button from "../../components/atom/button";
+import Input from "../../components/atom/input";
 // import Dropdown from "../../components/atom/dropdown";
-// import Input from "../../components/atom/input";
 
 export default function Index() {
   return (
     <div>
       Home
-      <Button size="large" onClick={() => {}}>
+      {/* <Button size="large" onClick={() => {}}>
         button
-      </Button>
+      </Button> */}
+      <Input
+        id="input"
+        value=""
+        message="메시지 내용"
+        isError={false}
+        size="small"
+        placeholder="플레이스홀더"
+        onChange={() => {}}
+      ></Input>
     </div>
   );
 }

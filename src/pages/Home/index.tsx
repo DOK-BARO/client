714256672483d2c40e2fa4fa82c1febb96d2de51
page1597 <gimm08377@gmail.com>
2024-07-13 23:24:36 +0900

    Design(DK-66): Textarea 스타일링 적용

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,23 +1,36 @@
 // import Button from "../../components/atom/button";
 import Input from "../../components/atom/input";
+import Textarea from "../../components/atom/textarea";
 // import Dropdown from "../../components/atom/dropdown";
 
 export default function Index() {
   return (
-    <div>
+    <div style={{ margin: "10%" }}>
       Home
       {/* <Button size="large" onClick={() => {}}>
         button
       </Button> */}
-      <Input
-        id="input"
-        value=""
-        message="메시지 내용"
-        isError={false}
-        size="small"
-        placeholder="플레이스홀더"
-        onChange={() => {}}
-      ></Input>
+      <div style={{ display: "flex", gap: "30px" }}>
+        <Input
+          id="input"
+          value=""
+          message="메시지 내용"
+          isError={false}
+          size="small"
+          // disabled
+          placeholder="플레이스홀더"
+          onChange={() => {}}
+        ></Input>
+        <Textarea
+          id="textarea"
+          value=""
+          // disabled
+          placeholder="플레이스홀더"
+          onChange={() => {}}
+          isError={false}
+          message="메시지 내용"
+        ></Textarea>
+      </div>
     </div>
   );
 }

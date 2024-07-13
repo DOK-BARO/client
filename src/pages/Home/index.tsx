fbe9a1d3091eaa055bf7dc895fb50220e2a689eb
hyerimRoh <mtxpvksh@gmail.com>
2024-07-14 02:55:59 +0900

    feat(DK-66): 공용 컴포넌트 Modal 및 modal state를 관리하는 useModal hook 구현

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,12 +1,24 @@
 // import Button from "../../components/atom/button";
 import Input from "../../components/atom/input";
 import Textarea from "../../components/atom/textarea";
+import useModal from "../../hooks/useModal.tsx";
+import Modal from "../../components/atom/modal.tsx";
 // import Dropdown from "../../components/atom/dropdown";
 
 export default function Index() {
+  const { isModalOpen,openModal, closeModal } = useModal();
   return (
     <div style={{ margin: "10%" }}>
       Home
+      <button onClick={openModal}>팝업 열기</button>
+      {isModalOpen 
+          && 
+          <Modal 
+            popUpTitle="팝업 타이틀"
+            contentTitle="내용 타이틀"
+            content="대화상자는 사용자에게 작업에대해 알리고 중요한 정보를~"
+            closeModal={closeModal}
+          />}
       {/* <Button size="large" onClick={() => {}}>
         button
       </Button> */}

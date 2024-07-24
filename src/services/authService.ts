ab55780871d29aeb91d2fda8d38fc26309222838
page1597 <gimm08377@gmail.com>
2024-07-25 00:40:32 +0900

    Fix(DK-1): 카카오 회원가입  응답 URL로 리다이렉트되도록 변경

--- src/services/authService.ts
+++ src/services/authService.ts
@@ -3,9 +3,9 @@ import axios from "axios";
 export const getKakaoAuthUrl = async (): Promise<string> => {
   console.log("get kakao auth url");
   try {
-    const response = await axios.get("/auth/oauth2/authorize/KAKAO");
-    console.log(response); // 응답 객체 출력
-    return "/url"; // 임시 임의 리턴값
+    const { data } = await axios.get("/auth/oauth2/authorize/KAKAO");
+    console.log(data); // 응답 객체 출력
+    return data.url; // 임시 임의 리턴값
   } catch (error) {
     throw new Error(`권한 부여 URL 가져오기 실패: ${error}`);
   }

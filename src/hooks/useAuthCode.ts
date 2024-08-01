7dceb3ce46dae668e0a98393c9514d4c25a85c02
hyerimRoh <mtxpvksh@gmail.com>
2024-08-02 01:24:04 +0900

    Feat(DK-158): 서버로 로그인/회원가입 요청 처리 - 리다이렉트 후 로그인/회원가입 상태를 로컬스토리지에 저장하여 분기처리 - 서버로 로그인/회원가입 요청함수 구현

--- src/hooks/useAuthCode.ts
+++ src/hooks/useAuthCode.ts
@@ -1,13 +1,38 @@
 import { useEffect } from "react";
-import { useLocation } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
+import { loginByKakao, signupByKakao } from "../services/authService.ts";
+import { AuthResponse } from "../data/AuthResponse.ts";
 
 export const useAuthCode = () => {
   const location = useLocation();
+  const navigate = useNavigate();
+
+  const setToken = (result : AuthResponse) => {
+    localStorage.setItem("accessToken", result.accessToken);
+    localStorage.setItem("refreshToken", result.refreshToken);
+  };
+
+  const doSignUp = async (code: string) => {
+    const result = await signupByKakao(code);
+    setToken(result);
+    navigate("/");
+    localStorage.removeItem("authAction");
+  };
+
+  const doLogin = async (code: string) => {
+    const result = await loginByKakao(code);
+    setToken(result);
+    navigate("/");
+    localStorage.removeItem("authAction");
+  };
+
   useEffect(() => {
     const urlParams = new URLSearchParams(location.search);
     const code = urlParams.get("code");
+
     if (code) {
-      localStorage.setItem("authCode", code);
+      const action:string = localStorage.getItem("authAction")!;
+      action === "login" ? doLogin(code) : doSignUp(code);
     }
-  }, [location.search]);
+  }, []);
 };

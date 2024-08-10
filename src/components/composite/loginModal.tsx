37e5deb964b12a49a92247a46a12ec3186dd863b
hyerimRoh <mtxpvksh@gmail.com>
2024-08-11 02:24:54 +0900

    Style(DK-10): 소셜 로그인 / 회원가입 버튼 스타일링 - 카카오, 네이버, 구글, 깃허브 소셜 버튼 스타일링

--- src/components/composite/loginModal.tsx
+++ src/components/composite/loginModal.tsx
@@ -4,6 +4,9 @@ import CloseIcon from "@mui/icons-material/Close";
 import { gray80 } from "../../styles/abstracts/colors.ts";
 import React from "react";
 import HeaderLogo from "../atom/headerLogo.tsx";
+import { SOCIAL_TYPES } from "../../data/constants.ts";
+import SocialAuthButton from "./socialAuthButton.tsx";
+import { AuthType } from "../../types/AuthType.ts";
 
 interface LoginModalProps{
   closeModal: () => void;
@@ -24,12 +27,14 @@ const LoginModal:React.FC<LoginModalProps> = ({
             <HeaderLogo />
             <div className={styles["title-text"]}>더 효과적인 북 스터디를 위해</div>
           </div>
-          <div className={styles["content"]}>
-            <Button className={styles["github-auth-button"]} onClick={() => {}}>
-              <img src="/src/assets/svg/github-logo.svg" alt="깃허브 로그인 버튼"></img>
-              <div>깃허브계정으로 로그인</div>
-            </Button>
-          </div>
+          {SOCIAL_TYPES.map((socialType) => (
+            <SocialAuthButton
+              key={socialType}
+              authType={AuthType.LOGIN}
+              socialType={socialType}
+            />
+          ))
+          }
         </div>
       </div>
     </div>

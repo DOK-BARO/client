b3b85596501787d05e942f3b3a506e700b5b71e4
page1597 <gimm08377@gmail.com>
2024-09-03 16:41:25 +0900

    Style(DK-200): 헤더, 콘텐츠 너비 수정
    - 1140px

--- src/components/layout/headerLayout.tsx
+++ src/components/layout/headerLayout.tsx
@@ -4,12 +4,13 @@ import StartAuthButton from "../composite/startAuthButton.tsx";
 import HeaderUtilArea from "../composite/headerUtilArea.tsx";
 
 const HeaderLayout = ({ isLoggedIn }: { isLoggedIn: boolean }) => {
-
   return (
     <header className={styles["header-container"]}>
-      <HeaderLogo />
-      <HeaderUtilArea isLoggedIn={isLoggedIn}/>
-      <StartAuthButton isLoggedIn={isLoggedIn}/>
+      <div className={styles["inner-container"]}>
+        <HeaderLogo />
+        <HeaderUtilArea isLoggedIn={isLoggedIn} />
+        <StartAuthButton isLoggedIn={isLoggedIn} />
+      </div>
     </header>
   );
 };

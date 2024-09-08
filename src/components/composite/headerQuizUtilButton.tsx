47cacccc6a6d3dae81417e15754aa2e2e573b8d5
hyerimRoh <mtxpvksh@gmail.com>
2024-09-08 13:52:18 +0900

    Feat(DK-200): 퀴즈도구 드롭다운 리스트 구현

--- src/components/composite/headerQuizUtilButton.tsx
+++ src/components/composite/headerQuizUtilButton.tsx
@@ -1,17 +1,23 @@
 import styles from "../../styles/components/_header_quiz_util_button.module.scss";
 import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
+import HeaderQuizUtilList from "./quizMenuList.tsx";
+import { useDropDownList } from "../../hooks/useDropDownList.ts";
 
 function HeaderQuizUtilButton ({ isLoggedIn }: { isLoggedIn : boolean }) {
+  const { isOpenDropDownList, anchorEl, openDropDownList, closeDropDownList, dropDownListRef } = useDropDownList();
 
   if (!isLoggedIn) {
     return null;
   }
   
   return (
-    <button className={styles["header-quiz-util"]}>
-      <h3>퀴즈 도구</h3>
-      <KeyboardArrowDownIcon className={styles["header-quiz-util-icon"]}/>
-    </button>
+    <div className={styles["header-quiz-util-button-container"]} ref={dropDownListRef}>
+      <button className={styles["header-quiz-util-button"]} onClick={openDropDownList}>
+        <h3>퀴즈 도구</h3>
+        <KeyboardArrowDownIcon className={styles["header-quiz-util-icon"]}/>
+      </button>
+      {isOpenDropDownList && anchorEl && <HeaderQuizUtilList closeDropDownList={closeDropDownList} />}
+    </div>
   );
 }
 

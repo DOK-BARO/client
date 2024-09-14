6f6129d9fa3a4a950ed6d1f6e3796eac89ef164d
page1597 <gimm08377@gmail.com>
2024-09-15 00:47:58 +0900

    Feat(DK-146): 약관 동의 체크 기능 추가

--- src/pages/Register/composite/termsAgreement.tsx
+++ src/pages/Register/composite/termsAgreement.tsx
@@ -1,6 +1,41 @@
+import { useState } from "react";
+import Button from "../../../components/atom/button";
 import styles from "../../../styles/composite/_terms_agreement.module.scss";
 
 export default function TermsAgreement() {
+  const [agreements, setAgreements] = useState({
+    allAgree: false,
+    termsAgree: false,
+    privacyAgree: false,
+    emailNews: false,
+    adsEmail: false,
+  });
+
+  const onAllAgreeChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const isChecked = event.target.checked;
+    setAgreements({
+      allAgree: isChecked,
+      termsAgree: isChecked,
+      privacyAgree: isChecked,
+      emailNews: isChecked,
+      adsEmail: isChecked,
+    });
+  };
+
+  const onSingleAgreeChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { id, checked } = event.target;
+    setAgreements((prev) => ({
+      ...prev,
+      [id]: checked,
+      allAgree:
+        prev.termsAgree &&
+        prev.privacyAgree &&
+        prev.emailNews &&
+        prev.adsEmail &&
+        checked,
+    }));
+  };
+
   return (
     <section className={styles["terms-agreement"]}>
       <h4 className={styles["description"]}>
@@ -12,9 +47,11 @@ export default function TermsAgreement() {
         {/* 모두 동의 */}
         <div>
           <input
-            id="all-agree"
+            id="allAgree"
             className={styles["all-agree"]}
             type="checkbox"
+            checked={agreements.allAgree}
+            onChange={onAllAgreeChange}
           />
           <label htmlFor="all-agree">모두 동의 (선택 정보 포함)</label>
         </div>
@@ -24,14 +61,26 @@ export default function TermsAgreement() {
         <fieldset>
           <legend>필수 약관</legend>
           <div>
-            <input id="terms-agree" type="checkbox" required />
-            <label htmlFor="terms-agree">[필수] 이용 약관 동의</label>
+            <input
+              id="termsAgree"
+              type="checkbox"
+              checked={agreements.termsAgree}
+              onChange={onSingleAgreeChange}
+              required
+            />
+            <label htmlFor="termsAgree">[필수] 이용 약관 동의</label>
             <button type="button">보기</button>
           </div>
 
           <div>
-            <input id="privacy-agree" type="checkbox" required />
-            <label htmlFor="privacy-agree">
+            <input
+              id="privacyAgree"
+              type="checkbox"
+              checked={agreements.privacyAgree}
+              onChange={onSingleAgreeChange}
+              required
+            />
+            <label htmlFor="privacyAgree">
               [필수] 개인 정보 수집·이용 동의
             </label>
             <button type="button">보기</button>
@@ -42,8 +91,13 @@ export default function TermsAgreement() {
         <fieldset>
           <legend>선택 약관</legend>
           <div>
-            <input id="email-news" type="checkbox" />
-            <label htmlFor="email-news">
+            <input
+              id="emailNews"
+              type="checkbox"
+              checked={agreements.emailNews}
+              onChange={onSingleAgreeChange}
+            />
+            <label htmlFor="emailNews">
               [선택] 신규 퀴즈 소식을 이메일로 받기
             </label>
             <button type="button">보기</button>
@@ -51,14 +105,23 @@ export default function TermsAgreement() {
           </div>
 
           <div>
-            <input id="ads-email" type="checkbox" />
-            <label htmlFor="ads-email">
+            <input
+              id="adsEmail"
+              type="checkbox"
+              checked={agreements.adsEmail}
+              onChange={onSingleAgreeChange}
+            />
+            <label htmlFor="adsEmail">
               [선택] DOKBARO의 광고 메시지를 이메일로 받기
             </label>
             <button type="button">보기</button>
             <p>DOKBARO가 고른 도서 소식을 이메일로 받아보세요!</p>
           </div>
         </fieldset>
+        {/* 버튼 사이즈 컨벤션 안맞음 */}
+        <Button size="large" onClick={() => {}}>
+          동의하고 가입하기
+        </Button>
       </form>
       <hr />
       <div></div>

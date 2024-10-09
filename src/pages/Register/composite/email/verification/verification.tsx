1401cb27b13d7b9689734444f65298c8d43d2cac
page1597 <gimm08377@gmail.com>
2024-10-09 15:39:05 +0900

    Style(DK-229): 인증코드 불일치 시 에러 메시지 표시

--- src/pages/Register/composite/email/verification/verification.tsx
+++ src/pages/Register/composite/email/verification/verification.tsx
@@ -1,6 +1,5 @@
 // 공용으로 사용되는 스타일 (파일명 바꾸기?)
 import styles from "./_verification.module.scss";
-import { Close } from "@/svg/close";
 import { systemDanger } from "@/styles/abstracts/colors";
 import { emailValidation } from "@/validation/emailValidation";
 import { useAtom } from "jotai";
@@ -9,8 +8,10 @@ import Input from "@/components/atom/input/input.tsx";
 import Button from "@/components/atom/button/button.tsx";
 import { RegisterInfoType } from "@/types/UserType";
 import { RegisterInfoAtom } from "@/store/userAtom";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import { XSmall } from "@/svg/xSmall";
 
 export default function Verification() {
   const [user, setUser] = useAtom<RegisterInfoType>(RegisterInfoAtom);
@@ -19,8 +20,10 @@ export default function Verification() {
     onChange: onEmailChange,
     isValid: isEmailValid,
   } = useInput(user.email, emailValidation);
-  // const navigate = useNavigate();
+
+  const navigate = useNavigate();
   const [code, setCode] = useState<string[]>(Array(6).fill(""));
+  const [isMatch, setIsMatch] = useState<boolean | undefined>(undefined);
   const fullCode: string = code.join("");
 
   const [isEmailSent, setIsEmailSent] = useState<boolean>(false);
@@ -98,16 +101,28 @@ export default function Verification() {
         email: email,
         code: code,
       });
+      // navigate("/register/email/3");
       // axios 타입으로 바꾸기
-      // if (response.status === 201) {
-      //   console.log(response);
-      //   navigate("/register/email/3");
-      // }
-      console.log(response);
+      if (response.status === 201) {
+        console.log(response);
+        setIsMatch(true);
+      }
+      console.log("response", response);
     } catch (e) {
-      console.error(e);
+      // 인증코드가 일치하지 않을 경우
+      // TODO: 상세한 에러 처리 필요
+      console.error("에러", e);
+      setIsMatch(false);
     }
   };
+
+  useEffect(() => {
+    if (isMatch) {
+      // 인증 코드가 일치할 경우에만 다음 페이지로 이동
+      navigate("/register/email/3");
+    }
+  }, [isMatch]);
+
   const handleKeyDown = (
     e: React.KeyboardEvent<HTMLInputElement>,
     index: number
@@ -152,7 +167,7 @@ export default function Verification() {
             isEmailValid === false ? (
               <span className={styles["message-container"]}>
                 <p>옳지 않은 형식의 이메일입니다.</p>
-                <Close stroke={systemDanger} width={20} height={20} />
+                <XSmall stroke={systemDanger} width={20} height={20} />
               </span>
             ) : (
               <></>
@@ -168,19 +183,30 @@ export default function Verification() {
           size="large"
         />
       ) : (
-        <div className={styles["code-input-container"]}>
-          {code.map((digit, i) => (
-            <Input
-              size="large"
-              key={i}
-              id={`code-input-${i}`}
-              value={digit}
-              onChange={(e) => onCodeChange(e, i)}
-              onKeyDown={(e) => handleKeyDown(e, i)}
-              maxLength={1}
-              color={fullCode.length !== 6 ? "default" : "black"}
-            />
-          ))}
+        <div className={styles["code-input-message-container"]}>
+          <div className={styles["code-input-container"]}>
+            {code.map((digit, i) => (
+              <Input
+                size="large"
+                key={i}
+                id={`code-input-${i}`}
+                value={digit}
+                onChange={(e) => onCodeChange(e, i)}
+                onKeyDown={(e) => handleKeyDown(e, i)}
+                maxLength={1}
+                color={fullCode.length !== 6 ? "default" : "black"}
+                isError={isMatch === false}
+              />
+            ))}
+          </div>
+          {isMatch === false ? (
+            <span className={styles["message-container"]}>
+              <p>인증 코드가 일치하지 않습니다.</p>
+              <XSmall stroke={systemDanger} width={20} height={20} />
+            </span>
+          ) : (
+            <></>
+          )}
         </div>
       )}
       <Button

2b4bcbdc0c885056e8a55cc380ecbbb83f81d52d
page1597 <gimm08377@gmail.com>
2024-10-13 14:55:13 +0900

    Feat(DK-246): 스터디 선택 기능 수정

--- src/pages/CreateQuiz/composite/quizSettingStudyGroupForm/quizSettingStudyGroupForm.tsx
+++ src/pages/CreateQuiz/composite/quizSettingStudyGroupForm/quizSettingStudyGroupForm.tsx
@@ -2,7 +2,6 @@ import styles from "./_quiz_setting_study_group_form.module.scss";
 import { useState } from "react";
 import useModal from "@/hooks/useModal.ts";
 import useInput from "@/hooks/useInput.ts";
-import useTextarea from "@/hooks/useTextarea.ts";
 import Button from "@/components/atom/button/button.tsx";
 import { primary } from "@/styles/abstracts/colors.ts";
 import Modal from "@/components/atom/modal/modal.tsx";
@@ -11,40 +10,40 @@ import { QuizPlus } from "@/svg/quizPlus";
 import { XMedium } from "@/svg/xMedium";
 import { Link } from "@/svg/link";
 import { Copy } from "@/svg/copy";
+import { StudyGroupType } from "@/types/StudyGroupType";
 
-// 1.스터디 생성 / 선택
+// 1.스터디 선택
 export default function QuizSettingStudyGroupForm() {
   // TODO: custom hook으로 분리하기
   const { isModalOpen, openModal, closeModal } = useModal();
-  const [selectedStudy, setSelectedStudy] = useState<string>("");
+  // const [selectedStudy, setSelectedStudy] = useState<string | null>(null);
+
+  const [studyGroupList, setStudyGroupList] = useState<StudyGroupType[]>([]);
+  const [selectedStudyGroupList, setSelectedStudyGroupList] = useState<
+    StudyGroupType[]
+  >([]);
+  const [newStudyGroup, setNewStudyGroup] = useState<string | null>(null);
 
   // 모달 안 인풋
   const {
     value: studyName,
     onChange: onChangeStudyName,
-    resetInput,
+    resetInput: resetStudyNameInput,
   } = useInput("");
   const { value: inviteEmail, onChange: onChangeInviteEmail } = useInput("");
-
-  const { value: quizTitle, onChange: onChangeQuizTitle } = useInput("");
-  const { value: quizDesc, onChange: onChangeQuizDesc } = useTextarea("");
-
-  const profileImgSrc = "";
-
-  const [isStudySelected, setIsStudySelected] = useState<boolean>(false);
 
   const [isToggleOn, setIsToggleOn] = useState<boolean>(false);
 
   // 새로운 스터디 그룹 추가
-  const addStudyGroup = (studyNameInputValue: string) => {
-    setSelectedStudy(studyNameInputValue);
+  const addStudyGroup = (newStudyName: string) => {
+    setNewStudyGroup(newStudyName);
   };
 
   // 입력한 스터디 그룹 삭제
   const removeStudyGroup = () => {
-    setSelectedStudy("");
+    setNewStudyGroup("");
     // 인풋 value도 초기화
-    resetInput("");
+    resetStudyNameInput("");
   };
 
   // 이메일을 통해 스터디 그룹에 초대하기
@@ -64,8 +63,17 @@ export default function QuizSettingStudyGroupForm() {
 
   // 완료. (모달창 닫기)
   const done = () => {
-    setIsStudySelected(true);
-    console.log(selectedStudy);
+    // 확인 눌렀을때. // api 요청 해야됨
+    if (newStudyGroup) {
+      setStudyGroupList([
+        ...studyGroupList,
+        {
+          id: -1, // 임시
+          name: newStudyGroup,
+        },
+      ]);
+    }
+
     closeModal();
   };
 
@@ -76,44 +84,64 @@ export default function QuizSettingStudyGroupForm() {
 
   return (
     <>
-      {isStudySelected && (
+      {studyGroupList.length > 0 && (
         <>
           <p className={styles["email-notification"]}>
             스터디원들의 이메일로 퀴즈 생성 알림이 가요.
           </p>
-          <article className={styles["selected-study-group"]}>
-            <div className={styles["profile-container"]}>
-              {profileImgSrc ? (
-                <img className={styles.profile} src="" alt="" />
-              ) : (
-                <div className={styles.profile} />
-              )}
-              {selectedStudy}
-            </div>
-            {/* toggle button -> 분리하기 */}
-            <div
-              className={`${styles["toggle-container"]} ${
-                isToggleOn ? styles["on"] : styles["off"]
+          {studyGroupList.map((studyGroup) => (
+            <article
+              id={studyGroup.id.toString()}
+              className={`${styles["study-group"]} ${
+                selectedStudyGroupList.includes(studyGroup)
+                  ? styles["selected"]
+                  : styles["non"]
               }`}
+              onClick={() => {
+                setSelectedStudyGroupList((prevList) =>
+                  prevList.includes(studyGroup)
+                    ? prevList.filter((item) => item !== studyGroup)
+                    : [...prevList, studyGroup]
+                );
+              }}
             >
-              <button
-                className={`${styles["toggle"]} ${
+              <div className={styles["profile-container"]}>
+                {studyGroup.profileImageUrl ? (
+                  <img className={styles.profile} src="" alt="" />
+                ) : (
+                  <div className={styles.profile} />
+                )}
+                {studyGroup.name}
+              </div>
+              {/* toggle button -> 분리하기 */}
+              <div
+                className={`${styles["toggle-container"]} ${
                   isToggleOn ? styles["on"] : styles["off"]
                 }`}
-                value={isToggleOn.toString()}
-                onClick={onToggle}
               >
-                <div className={styles["circle"]} />
-              </button>
-            </div>
-          </article>
+                <button
+                  className={`${styles["toggle"]} ${
+                    isToggleOn ? styles["on"] : styles["off"]
+                  }`}
+                  value={isToggleOn.toString()}
+                  onClick={onToggle}
+                >
+                  <div className={styles["circle"]} />
+                </button>
+              </div>
+            </article>
+          ))}
         </>
       )}
 
       <Button
         size="large"
         className={styles["add-study-group"]}
-        onClick={openModal}
+        onClick={() => {
+          openModal();
+          setNewStudyGroup(null);
+          resetStudyNameInput("");
+        }}
         icon={
           <QuizPlus
             alt="스터디 그룹 추가 버튼"
@@ -133,7 +161,7 @@ export default function QuizSettingStudyGroupForm() {
           contentTitle="새로운 스터디 그룹 이름"
           content={
             <div className={styles["add-study-name"]}>
-              {selectedStudy && (
+              {newStudyGroup && (
                 <Button
                   onClick={() => {}}
                   iconPosition="right"
@@ -149,11 +177,11 @@ export default function QuizSettingStudyGroupForm() {
                   className={styles["study-name"]}
                   color="secondary"
                 >
-                  {selectedStudy}
+                  {newStudyGroup}
                 </Button>
               )}
 
-              {!selectedStudy ? (
+              {!newStudyGroup ? (
                 // 스터디 그룹이 입력되어 있지 않은 경우 (초기 상태)
                 <div className={styles["input-button-container"]}>
                   <Input

f96aad0e053f8c8e460ccd48d05f961b922a4eb7
hyerimRoh <mtxpvksh@gmail.com>
2024-10-20 02:20:42 +0900

    Fix(DK-249): svg 포맷 컴포넌트들에서 속성 에러나는 문제 해결 - 케밥 케이스 -> 카멜케이스로 속성명칭 변셩

--- src/svg/quizWriteForm/ulList.tsx
+++ src/svg/quizWriteForm/ulList.tsx
@@ -13,7 +13,7 @@ export const UlList: React.FC<SVGProps> = ({ alt, ...props }) => {
       <title id="svgTitle">{alt}</title>
       <path
         d="M8.71961 6H21.5996M8.71961 12.48H21.5996M8.71961 18.96H21.5996M3.59961 6V6.0128M3.59961 12.48V12.4928M3.59961 18.96V18.9728"
-        stroke="#1D1D1D" stroke-width="2.4" stroke-linecap="round" stroke-linejoin="round"/>
+        stroke="#1D1D1D" strokeWidth="2.4" strokeLinecap="round" strokeLinejoin="round"/>
     </svg>
   );
 };

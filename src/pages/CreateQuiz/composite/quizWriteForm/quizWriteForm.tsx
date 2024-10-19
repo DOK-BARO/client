fec48ce97ead39ca0e5fd5733a4e2192c3a89b89
hyerimRoh <mtxpvksh@gmail.com>
2024-10-20 04:09:04 +0900

    Fix(DK-249): 퀴즈 작성 - 퀴즈 작성 폼 리스트에서 삭제 구현

--- src/pages/CreateQuiz/composite/quizWriteForm/quizWriteForm.tsx
+++ src/pages/CreateQuiz/composite/quizWriteForm/quizWriteForm.tsx
@@ -1,120 +1,44 @@
-import React, { ReactNode, useState } from "react";
+import { ReactNode, useState } from "react";
 import styles from "./_quiz_write_form.module.scss";
-import useInput from "@/hooks/useInput.ts";
 import Button from "@/components/atom/button/button.tsx";
-import Input from "@/components/atom/input/input.tsx";
-import QuizWriteFormTypeUtilButton from "@/pages/CreateQuiz/composite/quizWriteForm/quizWriteFormTypeUtilButton.tsx";
+import QuizWriteFormItem from "@/pages/CreateQuiz/composite/quizWriteForm/quizWriteFormItem.tsx";
 
 // 3. 퀴즈 작성
-interface QuizWriteForm {
+export interface QuizWriteFormItemType {
   id: number;
   component: ReactNode;
 }
 
-
 export default function QuizWriteForm() {
-  const [quizWriteFormList, setQuizWriteList] = useState<QuizWriteForm[]>([{ id: Date.now(), component: <QuizWriteFormItem />,
-  }]);
+  const [quizWriteFormList, setQuizWriteList] = useState<QuizWriteFormItemType[]>([]);
+  
+  const deleteQuizWriteForm = (targetId: number) => {
+    console.log(targetId);
+
+    setQuizWriteList((prevQuizList)=> {
+      return prevQuizList.filter(({ id }) => id !== targetId);
+    });
+
+  };
+  
   const onClickAddQuizWriteForm = () => {
     const id = Date.now();
-    //setQuizWriteList([...quizWriteFormList, <QuizWriteFormItem />]);
     setQuizWriteList((prev) => [
       ...prev,
-      { id: id, component: <QuizWriteFormItem /> },
+      { id: id, component: <QuizWriteFormItem id={id} deleteQuizWriteForm={deleteQuizWriteForm}/> },
     ]);
   };
 
+
+
   return (
     <div className={styles["container"]}>
-      {quizWriteFormList.map(({ id, component }) => (
-        <div key={id}>{component}</div>
+      {quizWriteFormList.map((item) => (
+        <div key={item.id}>{item.component}</div>
       ))}
       <Button size={"xlarge"} onClick={onClickAddQuizWriteForm}>
         문제 추가하기 +
       </Button>
-    </div>
-  );
-}
-
-
-function QuizWriteFormItem () {
-  const [quizMode, setQuizMode] = useState<string>("question"); // 질문 작성 or 답안 작성
-  const { onChange: onQuestionChange, value: question } = useInput("");
-  const { onChange: onOptionChange, value: option, resetInput } = useInput("");
-  const [optionList, setOptionList] = useState<string[]>([]);
-
-  const onQuizModeSelect = (e: React.MouseEvent<HTMLButtonElement>) => {
-    setQuizMode(e.currentTarget.value);
-  };
-
-  // 옵션 추가 인풋창에 옵션(선지) 입력 후 엔터 눌렀을 때 등록
-  const onEnter = (e: React.KeyboardEvent<HTMLInputElement>) => {
-    e.stopPropagation();
-    if (e.key === "Enter") {
-      setOptionList([...optionList, e.currentTarget.value]);
-      resetInput("");
-    }
-  };
-
-  return (
-    <div className={styles["write-quiz"]}>
-      <div className={styles["quiz-mode-buttons"]}>
-        <Button
-          value="question"
-          size="medium"
-          className={`${styles["set-quiz"]} ${
-            quizMode === "question" ? styles["active"] : ""
-          }`}
-          onClick={onQuizModeSelect}
-        >
-          질문 설정
-        </Button>
-        <Button
-          value="answer"
-          size="medium"
-          className={`${styles["set-answer"]} ${
-            quizMode === "answer" ? styles["active"] : ""
-          }`}
-          onClick={onQuizModeSelect}
-        >
-          답안 설정
-        </Button>
-      </div>
-      <div className={styles["input-container"]}>
-
-        <QuizWriteFormTypeUtilButton />
-
-        <Input
-          className={styles["question"]}
-          value={question}
-          onChange={onQuestionChange}
-          id="option"
-          placeholder="질문을 입력해주세요."
-        />
-      </div>
-      {/* TODO: 컴포넌트 분리하기 */}
-      <fieldset className={styles["question-options"]}>
-        <legend>답안 선택지</legend>
-
-        {optionList.map((option: string) => (
-          <div key={option} className={styles["option-container"]}>
-            <div className={styles["option-check-circle"]}/>
-            <span className={styles["option-label"]}>{option}</span>
-          </div>
-        ))}
-        <div className={styles["option-container"]}>
-          <div className={styles["option-check-circle"]}/>
-          <input
-            id="option"
-            value={option}
-            onKeyDown={onEnter}
-            onChange={onOptionChange}
-            className={styles["new-option"]}
-            placeholder="옵션 추가하기"
-            autoFocus
-          />
-        </div>
-      </fieldset>
     </div>
   );
 }
---
+++ src/pages/CreateQuiz/composite/quizWriteForm/quizWriteFormItem.tsx
@@ -0,0 +1,104 @@
+import React, { useState } from "react";
+import useInput from "@/hooks/useInput.ts";
+import styles from "./_quiz_write_form_item.module.scss";
+import Button from "@/components/atom/button/button.tsx";
+import { Move } from "@/svg/quizWriteForm/move.tsx";
+import { gray60, gray90 } from "@/styles/abstracts/colors.ts";
+import { Trash } from "@/svg/quizWriteForm/trash.tsx";
+import QuizWriteFormTypeUtilButton from "@/pages/CreateQuiz/composite/quizWriteForm/quizWriteFormTypeUtilButton.tsx";
+import Input from "@/components/atom/input/input.tsx";
+
+interface QuizWriteFormItemProps {
+  id: number;
+  deleteQuizWriteForm: (id: number) => void;
+}
+
+export default function QuizWriteFormItem ({ id, deleteQuizWriteForm }: QuizWriteFormItemProps) {
+  const [quizMode, setQuizMode] = useState<string>("question"); // 질문 작성 or 답안 작성
+  const { onChange: onQuestionChange, value: question } = useInput("");
+  const { onChange: onOptionChange, value: option, resetInput } = useInput("");
+  const [optionList, setOptionList] = useState<string[]>([]);
+
+  const onQuizModeSelect = (e: React.MouseEvent<HTMLButtonElement>) => {
+    setQuizMode(e.currentTarget.value);
+  };
+
+  // 옵션 추가 인풋창에 옵션(선지) 입력 후 엔터 눌렀을 때 등록
+  const onEnter = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    e.stopPropagation();
+    if (e.key === "Enter") {
+      setOptionList([...optionList, e.currentTarget.value]);
+      resetInput("");
+    }
+  };
+
+  return (
+    <div className={styles["write-quiz"]}>
+      <div className={styles["write-quiz-header"]}>
+        <div className={styles["quiz-mode-buttons"]}>
+          <Button
+            value="question"
+            size="medium"
+            className={`${styles["set-quiz"]} ${
+              quizMode === "question" ? styles["active"] : ""
+            }`}
+            onClick={onQuizModeSelect}
+          >
+            질문 설정
+          </Button>
+          <Button
+            value="answer"
+            size="medium"
+            className={`${styles["set-answer"]} ${
+              quizMode === "answer" ? styles["active"] : ""
+            }`}
+            onClick={onQuizModeSelect}
+          >
+            답안 설정
+          </Button>
+        </div>
+        <span>
+          <Move width={24} height={24} stroke={gray60}/>
+          <button onClick={() => deleteQuizWriteForm(id)}>
+            <Trash width={24} height={24} stroke={gray90}/>
+          </button>
+        </span>
+      </div>
+      <div className={styles["input-container"]}>
+
+        <QuizWriteFormTypeUtilButton />
+
+        <Input
+          className={styles["question"]}
+          value={question}
+          onChange={onQuestionChange}
+          id="option"
+          placeholder="질문을 입력해주세요."
+        />
+      </div>
+      {/* TODO: 컴포넌트 분리하기 */}
+      <fieldset className={styles["question-options"]}>
+        <legend>답안 선택지</legend>
+
+        {optionList.map((option: string) => (
+          <div key={option} className={styles["option-container"]}>
+            <div className={styles["option-check-circle"]}/>
+            <span className={styles["option-label"]}>{option}</span>
+          </div>
+        ))}
+        <div className={styles["option-container"]}>
+          <div className={styles["option-check-circle"]}/>
+          <input
+            id="option"
+            value={option}
+            onKeyDown={onEnter}
+            onChange={onOptionChange}
+            className={styles["new-option"]}
+            placeholder="옵션 추가하기"
+            autoFocus
+          />
+        </div>
+      </fieldset>
+    </div>
+  );
+}

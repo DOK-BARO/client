507f3b4417fb3d19533a19a673269755bf6a9864
hyerimRoh <mtxpvksh@gmail.com>
2024-10-21 23:21:58 +0900

    Feat(DK-249): 퀴즈 작성 폼 - 객관식 옵션 구현 - 옵션 추가하기 버튼 클릭 시 객관식 옵션이 리스트에 추가됨 - 답안 설정버튼 클릭 시 보이는 답안 작성 컴포넌트 초기 구현

--- src/pages/CreateQuiz/composite/quizWriteForm/quizWriteFormItem.tsx
+++ src/pages/CreateQuiz/composite/quizWriteForm/quizWriteFormItem.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { ReactNode, useState } from "react";
 import useInput from "@/hooks/useInput.ts";
 import styles from "./_quiz_write_form_item.module.scss";
 import Button from "@/components/atom/button/button.tsx";
@@ -7,30 +7,63 @@ import { Trash } from "@/svg/quizWriteForm/trash.tsx";
 import QuizWriteFormTypeUtilButton from "@/pages/CreateQuiz/composite/quizWriteForm/quizWriteFormTypeUtilButton.tsx";
 import Input from "@/components/atom/input/input.tsx";
 import { gray60, gray90 } from "@/styles/abstracts/colors.ts";
+import { Close } from "@/svg/close.tsx";
+import Textarea from "@/components/atom/textarea/textarea.tsx";
+import useTextarea from "@/hooks/useTextarea.ts";
+import { ImageAdd } from "@/svg/quizWriteForm/imageAdd.tsx";
 
 interface QuizWriteFormItemProps {
   id: number;
   deleteQuizWriteForm: (id: number) => void;
 }
 
-export default function QuizWriteFormItem ({ id, deleteQuizWriteForm }: QuizWriteFormItemProps) {
+interface QuizOptionItemType {
+  id: number;
+  component: ReactNode;
+}
+
+export default function QuizWriteFormItem({ id, deleteQuizWriteForm }: QuizWriteFormItemProps) {
   const [quizMode, setQuizMode] = useState<string>("question"); // 질문 작성 or 답안 작성
   const { onChange: onQuestionChange, value: question } = useInput("");
-  const { onChange: onOptionChange, value: option, resetInput } = useInput("");
-  const [optionList, setOptionList] = useState<string[]>([]);
+  const [optionList, setOptionList] = useState<QuizOptionItemType[]>([]);
+  const [focusedOptionIndex, setFocusedOptionIndex] = useState<number | null>(null);
+  const { value:answerTextAreaValue,onChange:onAnswerTextAreaChange }= useTextarea("");
 
   const onQuizModeSelect = (e: React.MouseEvent<HTMLButtonElement>) => {
+    //TODO: 답안 설정 클릭 시
+    // TODO : 옵션리스트 텍스트 라벨 못씀
+    //TODO: 답안 설명란 생김
+    // TODO: 옵션 리스트 클릭 시 답안으로 설정됨
     setQuizMode(e.currentTarget.value);
   };
 
-  // 옵션 추가 인풋창에 옵션(선지) 입력 후 엔터 눌렀을 때 등록
-  const onEnter = (e: React.KeyboardEvent<HTMLInputElement>) => {
-    e.stopPropagation();
-    if (e.key === "Enter") {
-      setOptionList([...optionList, e.currentTarget.value]);
-      resetInput("");
-    }
+  const onClickAddQuizOptionItem = () => {
+    const id = Date.now();
+    setOptionList((prev) => [
+      ...prev,
+      {
+        id: id,
+        component: <QuizWriteFormOptionItem id={id} deleteOption={deleteOption} focusedOptionIndex={focusedOptionIndex}
+          handleOptionFocus={handleOptionFocus}
+          handleOptionBlur={handleOptionBlur}                                  
+        />,
+      },
+    ]);
   };
+
+  const handleOptionFocus = (id: number) => {
+    console.log(id);
+    setFocusedOptionIndex(id);
+  };
+
+  const handleOptionBlur = () => {
+    setFocusedOptionIndex(null);
+  };
+
+  const deleteOption = (optionId: number) => {
+    setOptionList(optionList.filter((item) => item.id !== optionId));
+  };
+
 
   return (
     <div className={styles["write-quiz"]}>
@@ -60,7 +93,8 @@ export default function QuizWriteFormItem ({ id, deleteQuizWriteForm }: QuizWrit
         <span>
           <button
             className={styles["move-quiz-button"]}
-            onClick={() => {}}>
+            onClick={() => {
+            }}>
             <Move width={24} height={24} stroke={gray60}/>
           </button>
           <button
@@ -70,10 +104,9 @@ export default function QuizWriteFormItem ({ id, deleteQuizWriteForm }: QuizWrit
           </button>
         </span>
       </div>
+
       <div className={styles["input-container"]}>
-
-        <QuizWriteFormTypeUtilButton />
-
+        <QuizWriteFormTypeUtilButton/>
         <Input
           className={styles["question"]}
           value={question}
@@ -82,29 +115,89 @@ export default function QuizWriteFormItem ({ id, deleteQuizWriteForm }: QuizWrit
           placeholder="질문을 입력해주세요."
         />
       </div>
-      {/* TODO: 컴포넌트 분리하기 */}
+
       <fieldset className={styles["question-options"]}>
         <legend>답안 선택지</legend>
+        {optionList.map((item) =>
+          <QuizWriteFormOptionItem
+            id={item.id}
+            deleteOption={deleteOption} focusedOptionIndex={focusedOptionIndex}
+            handleOptionFocus={handleOptionFocus}
+            handleOptionBlur={handleOptionBlur}
+          />,
+        )}
+        <AddOptionButton onAdd={onClickAddQuizOptionItem}/>
+      </fieldset>
 
-        {optionList.map((option: string) => (
-          <div key={option} className={styles["option-container"]}>
-            <div className={styles["option-check-circle"]}/>
-            <span className={styles["option-label"]}>{option}</span>
+
+      {
+        quizMode === "answer" && 
+        <div>
+          <div>
+            <span>답안 설명</span>
+            <button><ImageAdd width={24} height={24}/></button>
           </div>
-        ))}
-        <div className={styles["option-container"]}>
-          <div className={styles["option-check-circle"]}/>
-          <input
-            id="option"
-            value={option}
-            onKeyDown={onEnter}
-            onChange={onOptionChange}
-            className={styles["new-option"]}
-            placeholder="옵션 추가하기"
-            autoFocus
+          <Textarea
+            id={"answer"}
+            onChange={onAnswerTextAreaChange}
+            value={answerTextAreaValue}
+            placeholder={"답안에 대한 설명을 입력해주세요"}
           />
         </div>
-      </fieldset>
+      }
+    </div>
+  );
+}
+
+
+function AddOptionButton({ onAdd }: { onAdd: () => void }) {
+
+  return (
+    <div className={styles["option-add-button-container"]}>
+      <button
+        className={styles["option-add-button"]}
+        onClick={onAdd}>
+        <div className={styles["option-add-button-check-circle"]} />
+        <span>옵션 추가하기</span>
+      </button>
+    </div>
+  );
+}
+
+interface QuizOptionItemProps {
+  id: number;
+  focusedOptionIndex: number | null;
+  handleOptionFocus: (id: number) => void;
+  handleOptionBlur: ( )=> void;
+  deleteOption: (id: number) => void;
+}
+
+function QuizWriteFormOptionItem({ id, focusedOptionIndex, deleteOption, handleOptionFocus, handleOptionBlur }: QuizOptionItemProps) {
+  const { onChange: onOptionChange, value: option } = useInput("");
+
+  return (
+    <div key={id}
+      className={`${styles["option-container"]} ${focusedOptionIndex === id ? styles["focused"] : ""}`}
+      onFocus={() => handleOptionFocus(id)}
+      onBlur={handleOptionBlur}
+    >
+      <div className={styles["option-label"]}>
+        <div className={styles["option-check-circle"]}/>
+        <input
+          id="option"
+          value={option}
+          onChange={onOptionChange}
+          className={`${styles["new-option"]} ${focusedOptionIndex === id ? styles["focused"] : ""}`}
+          // placeholder="선택지를 입력해주세요"
+          autoFocus
+        />
+      </div>
+      <button
+        className={styles["delete-option-button"]}
+        onClick={() => deleteOption(id)}
+      >
+        <Close width={20} height={20} stroke={gray90} strokeWidth={2}/>
+      </button>
     </div>
   );
 }

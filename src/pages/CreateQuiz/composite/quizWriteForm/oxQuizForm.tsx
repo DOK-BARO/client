de78b92d4ceaf667e47e14d71dc12ceca8403828
hyerimRoh <mtxpvksh@gmail.com>
2024-10-31 23:54:26 +0900

    Style(DK-249): 퀴즈 작성 폼 - ox 입력 폼 스타일링

--- src/pages/CreateQuiz/composite/quizWriteForm/oxQuizForm.tsx
+++ src/pages/CreateQuiz/composite/quizWriteForm/oxQuizForm.tsx
@@ -1,6 +1,6 @@
-import styles from "@/pages/CreateQuiz/composite/quizWriteForm/_quiz_write_form_item.module.scss";
+import styles from "./_ox_quiz_form.module.scss";
 import { RadioOption } from "@/types/RadioTypes.ts";
-import { FC } from "react";
+import { FC, useState } from "react";
 import { QuizFormMode } from "@/data/constants.ts";
 import RadioButton from "@/components/atom/radioButton/radioButton.tsx";
 import useRadioGroup from "@/hooks/useRadioGroup.ts";
@@ -19,11 +19,26 @@ export const OXQuizForm : FC<{quizMode?: string}> = ({ quizMode }) => {
   }];
   const { selectedValue: selectedRadioGroupValue, handleChange: handleRadioGroupChange } = useRadioGroup("");
   const disabled : boolean = quizMode === QuizFormMode.QUESTION;
+  const [focusedOptionIndex, setFocusedOptionIndex] = useState<number | null>(null);
+
+  const handleOptionFocus = (id: number) => {
+    setFocusedOptionIndex(id);
+  };
+
+  const handleOptionBlur = () => {
+    setFocusedOptionIndex(null);
+  };
   
   return (
     <fieldset className={styles["question-options"]}>
       <legend>답안 선택지</legend>
-      {options.map((option) =>
+      {
+      options.map((option) =>
+      <div 
+      className={`${styles["option-container"]} ${focusedOptionIndex === option.id ? styles["focused"] : ""}`}
+      onFocus={() => handleOptionFocus(option.id)}
+      onBlur={handleOptionBlur}
+      >
         <RadioButton
           option={option}
           selectedValue={selectedRadioGroupValue}
@@ -33,7 +48,8 @@ export const OXQuizForm : FC<{quizMode?: string}> = ({ quizMode }) => {
           LabelComponent={
             <div className={`${styles["new-option-label"]}`}>{option.value}</div>
           }
-        />,
+        />
+        </div>
       )}
     </fieldset>
   );

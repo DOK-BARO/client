5bdc2188b174fc7a60709796f1cf5e9468773f58
hyerimRoh <mtxpvksh@gmail.com>
2024-11-03 13:38:46 +0900

    Fix(DK-249): 라디오 그룹 id 누락된 부분 수정

--- src/pages/ComponentTest/index.tsx
+++ src/pages/ComponentTest/index.tsx
@@ -58,6 +58,7 @@ export default function Index() {
 
           return <RadioButton
             key={option.id}
+            radioGroupName={"radio-group"}
             option={option}
             selectedValue={selectedValue}
             onChange={handleChange}

2fec96ba7d9305e1b85299917992e4cda359b0bc
page1597 <gimm08377@gmail.com>
2024-11-18 17:06:55 +0900

    Style(DK-303): 문제 작성 페이지 스타일링 수정

--- src/pages/CreateQuiz/composite/quizWriteForm/checkBoxQuizForm.tsx
+++ src/pages/CreateQuiz/composite/quizWriteForm/checkBoxQuizForm.tsx
@@ -2,50 +2,72 @@ import { FC, useState, useEffect } from "react";
 import styles from "@/pages/CreateQuiz/composite/quizWriteForm/_quiz_write_form_item.module.scss";
 import { QuizFormMode } from "@/data/constants.ts";
 import { CheckBoxOption } from "@/types/CheckBoxTypes.ts";
-import QuizWriteFormCheckBoxOptionItem
-  from "@/pages/CreateQuiz/composite/quizWriteForm/quizWriteFormCheckBoxOptionItem.tsx";
+import QuizWriteFormCheckBoxOptionItem from "@/pages/CreateQuiz/composite/quizWriteForm/quizWriteFormCheckBoxOptionItem.tsx";
 import { BookQuizQuestionType } from "@/types/BookQuizType";
 import useUpdateQuizCreationInfo from "@/hooks/useUpdateQuizCreationInfo";
 
 // TODO: multipleChoiceQuizForm과 겹치는 부분 리팩토링 필요
-export const CheckBoxQuizForm: FC<{ quizMode?: string, questionFormId?: string }> = ({ quizMode, questionFormId }) => {
-  const { quizCreationInfo, updateQuizCreationInfo } = useUpdateQuizCreationInfo();
+export const CheckBoxQuizForm: FC<{
+  quizMode?: string;
+  questionFormId?: string;
+}> = ({ quizMode, questionFormId }) => {
+  const { quizCreationInfo, updateQuizCreationInfo } =
+    useUpdateQuizCreationInfo();
 
-  const getQuestion = () => (quizCreationInfo.questions?.find((question) => (question.id.toString() === questionFormId)) as BookQuizQuestionType);
+  const getQuestion = () =>
+    quizCreationInfo.questions?.find(
+      (question) => question.id.toString() === questionFormId
+    ) as BookQuizQuestionType;
 
   const setInitialOptions = (): CheckBoxOption[] => {
     const question = getQuestion();
-    const initialOptions: CheckBoxOption[] = question?.selectOptions.map((option) => ({ id: option.id, value: option.value, label: option.option } as CheckBoxOption)) ?? [];
+    const initialOptions: CheckBoxOption[] =
+      question?.selectOptions.map(
+        (option) =>
+          ({
+            id: option.id,
+            value: option.value,
+            label: option.option,
+          } as CheckBoxOption)
+      ) ?? [];
     return initialOptions;
-  }
+  };
 
   const [options, setOptions] = useState<CheckBoxOption[]>(setInitialOptions());
-  const [focusedOptionIndex, setFocusedOptionIndex] = useState<number | null>(null);
+  const [focusedOptionIndex, setFocusedOptionIndex] = useState<number | null>(
+    null
+  );
 
   const setInitialAnswer = (): { [key: string]: boolean } => {
     const question = getQuestion();
-      const initCheckedOptions: { [key: string]: boolean } = {};
-      question.selectOptions.forEach(({ id, value }) => {
-        if (question.answers.includes(value)) { // 답안에 value가 포함되어 있으면
-          initCheckedOptions[id] = true; // 해당 선택지의 id를 true로 설정
-        }
-      })
-      return initCheckedOptions;
-  }
-  const [checkedOptions, setCheckedOptions] = useState<{ [key: string]: boolean }>(setInitialAnswer());
+    const initCheckedOptions: { [key: string]: boolean } = {};
+    question.selectOptions.forEach(({ id, value }) => {
+      if (question.answers.includes(value)) {
+        // 답안에 value가 포함되어 있으면
+        initCheckedOptions[id] = true; // 해당 선택지의 id를 true로 설정
+      }
+    });
+    return initCheckedOptions;
+  };
+  const [checkedOptions, setCheckedOptions] = useState<{
+    [key: string]: boolean;
+  }>(setInitialAnswer());
 
   const disabled: boolean = quizMode === QuizFormMode.QUESTION;
   const deleteOption = (optionId: number) => {
     setOptions(options.filter((option) => option.id !== optionId));
 
-    const updatedQuestions:BookQuizQuestionType[] = quizCreationInfo.questions?.map((question) => {
-      const filteredSelectOptions = question.selectOptions.filter((option) => option.id !== optionId);
-      return {
+    const updatedQuestions: BookQuizQuestionType[] =
+      quizCreationInfo.questions?.map((question) => {
+        const filteredSelectOptions = question.selectOptions.filter(
+          (option) => option.id !== optionId
+        );
+        return {
           ...question,
           selectOptions: filteredSelectOptions,
-      };
-  }) ?? [];
-    
+        };
+      }) ?? [];
+
     updateQuizCreationInfo("questions", updatedQuestions);
   };
 
@@ -68,16 +90,15 @@ export const CheckBoxQuizForm: FC<{ quizMode?: string, questionFormId?: string }
           ...question,
           selectOptions: [
             ...question.selectOptions,
-            { id: id, option: "", value: value},
+            { id: id, option: "", value: value },
           ],
         };
       }
       return question;
     });
 
-  updateQuizCreationInfo("questions",updatedQuestions);
+    updateQuizCreationInfo("questions", updatedQuestions);
   };
-
 
   const handleOptionFocus = (id: number) => {
     setFocusedOptionIndex(id);
@@ -100,17 +121,15 @@ export const CheckBoxQuizForm: FC<{ quizMode?: string, questionFormId?: string }
 
     const updatedQuestions = quizCreationInfo.questions!.map((question) =>
       question.id.toString() === questionFormId
-        ?
-        {
-          ...question,
-          answers: checked
-            ? [...question.answers, value]
-            : question.answers.filter((answer) => answer !== value),
-        }
-        :
-        question);
-    updateQuizCreationInfo("questions",updatedQuestions);
-
+        ? {
+            ...question,
+            answers: checked
+              ? [...question.answers, value]
+              : question.answers.filter((answer) => answer !== value),
+          }
+        : question
+    );
+    updateQuizCreationInfo("questions", updatedQuestions);
   };
 
   useEffect(() => {
@@ -120,24 +139,24 @@ export const CheckBoxQuizForm: FC<{ quizMode?: string, questionFormId?: string }
 
     if (quizMode === QuizFormMode.QUESTION) {
       question.selectOptions.forEach(({ id }) => {
-          initCheckedOptions[id] = false;
-      })
+        initCheckedOptions[id] = false;
+      });
       setCheckedOptions(initCheckedOptions);
     } else {
       question.selectOptions.forEach(({ id, value }) => {
-        if (question.answers.includes(value)) { // 답안에 value가 포함되어 있으면
+        if (question.answers.includes(value)) {
+          // 답안에 value가 포함되어 있으면
           initCheckedOptions[id] = true; // 해당 선택지의 id를 true로 설정
         }
-      })
+      });
       setCheckedOptions(initCheckedOptions);
     }
   }, [quizMode]);
 
   const setText = (optionId: number, label: string) => {
-
     const updatedOptions = options.map((option) => {
       if (option.id === optionId) {
-        return { ...option, label }
+        return { ...option, label };
       }
       return option;
     });
@@ -147,7 +166,7 @@ export const CheckBoxQuizForm: FC<{ quizMode?: string, questionFormId?: string }
   return (
     <fieldset className={styles["question-options"]}>
       <legend>답안 선택지</legend>
-      {options.map((option) =>
+      {options.map((option) => (
         <QuizWriteFormCheckBoxOptionItem
           key={option.id}
           option={option}
@@ -160,23 +179,20 @@ export const CheckBoxQuizForm: FC<{ quizMode?: string, questionFormId?: string }
           onChange={handleCheckboxChange}
           setText={setText}
           questionFormId={questionFormId!.toString()}
-          selectedValue={option.value} />,
-      )}
-      {
-        quizMode == QuizFormMode.QUESTION &&
+          selectedValue={option.value}
+        />
+      ))}
+      {quizMode == QuizFormMode.QUESTION && (
         <AddOptionButton onAdd={onClickAddQuizOptionItem} />
-      }
+      )}
     </fieldset>
   );
 };
 
 function AddOptionButton({ onAdd }: { onAdd: () => void }) {
-
   return (
     <div className={styles["option-add-button-container"]}>
-      <button
-        className={styles["option-add-button"]}
-        onClick={onAdd}>
+      <button className={styles["option-add-button"]} onClick={onAdd}>
         <div className={styles["option-add-button-check-square"]} />
         <span>옵션 추가하기</span>
       </button>
--- src/pages/CreateQuiz/composite/quizWriteForm/quizWriteForm.tsx
+++ src/pages/CreateQuiz/composite/quizWriteForm/quizWriteForm.tsx
@@ -2,38 +2,57 @@ import { useState } from "react";
 import styles from "./_quiz_write_form.module.scss";
 import Button from "@/components/atom/button/button.tsx";
 import QuizWriteFormItem from "@/pages/CreateQuiz/composite/quizWriteForm/quizWriteFormItem.tsx";
-import { DragDropContext, Draggable, Droppable, DropResult } from "react-beautiful-dnd";
+import {
+  DragDropContext,
+  Draggable,
+  Droppable,
+  DropResult,
+} from "react-beautiful-dnd";
 import { Plus } from "@/svg/plus.tsx";
 import { primary } from "@/styles/abstracts/colors.ts";
 import { BookQuizQuestionType } from "@/types/BookQuizType";
 import { QuizWriteFormItemType } from "@/types/BookQuizType";
 import useUpdateQuizCreationInfo from "@/hooks/useUpdateQuizCreationInfo";
+import { QuizPlus } from "@/svg/quizPlus";
 
 export default function QuizWriteForm() {
-  const { quizCreationInfo, updateQuizCreationInfo } = useUpdateQuizCreationInfo();
+  const { quizCreationInfo, updateQuizCreationInfo } =
+    useUpdateQuizCreationInfo();
 
   const deleteQuizWriteForm = (targetId: number) => {
+    setQuizWriteList((prevQuizList) =>
+      prevQuizList.filter(({ id }) => id !== targetId)
+    );
 
-    setQuizWriteList((prevQuizList) => (prevQuizList.filter(({ id }) => id !== targetId)));
-
-    const updatedQuestions = quizCreationInfo.questions?.filter(question => question.id !== targetId) ?? [];
-    updateQuizCreationInfo("questions",updatedQuestions);
+    const updatedQuestions =
+      quizCreationInfo.questions?.filter(
+        (question) => question.id !== targetId
+      ) ?? [];
+    updateQuizCreationInfo("questions", updatedQuestions);
   };
-  
 
   const setInitialForms = (): QuizWriteFormItemType[] => {
-    const quizWriteForms: QuizWriteFormItemType[] = 
-    quizCreationInfo.questions?.map((question) => ({
-        id: question.id,
-        quizWriteFormType: question.answerType,
-        component: <QuizWriteFormItem id={question.id} deleteQuizWriteForm={deleteQuizWriteForm} quizWriteFormType = {question.answerType}/>,
-      }
-    ) as QuizWriteFormItemType) ?? [];
+    const quizWriteForms: QuizWriteFormItemType[] =
+      quizCreationInfo.questions?.map(
+        (question) =>
+          ({
+            id: question.id,
+            quizWriteFormType: question.answerType,
+            component: (
+              <QuizWriteFormItem
+                id={question.id}
+                deleteQuizWriteForm={deleteQuizWriteForm}
+                quizWriteFormType={question.answerType}
+              />
+            ),
+          } as QuizWriteFormItemType)
+      ) ?? [];
 
     return quizWriteForms;
-  }
-  const [quizWriteFormList, setQuizWriteList] = useState<QuizWriteFormItemType[]>(setInitialForms());
-
+  };
+  const [quizWriteFormList, setQuizWriteList] = useState<
+    QuizWriteFormItemType[]
+  >(setInitialForms());
 
   const moveQuizWriteForm = (result: DropResult) => {
     if (!result.destination) return;
@@ -44,20 +63,23 @@ export default function QuizWriteForm() {
 
     //TODO: quiz, questions 이름 api와 통일 필요
     //TODO: 위의 겹치는 로직과 리팩토링 필요
-    const globalQuizItems = [...quizCreationInfo.questions??[]];
-    const [reorderedGlobalItem] = globalQuizItems.splice(result.source.index, 1);
-    globalQuizItems.splice(result.destination.index,0,reorderedGlobalItem);
+    const globalQuizItems = [...(quizCreationInfo.questions ?? [])];
+    const [reorderedGlobalItem] = globalQuizItems.splice(
+      result.source.index,
+      1
+    );
+    globalQuizItems.splice(result.destination.index, 0, reorderedGlobalItem);
     updateQuizCreationInfo("questions", globalQuizItems);
   };
 
   const createNewQuestion = (id: number): BookQuizQuestionType => ({
-      id,
-      content: "",
-      selectOptions: [],
-      answerExplanationContent: "",
-      answerType: "MULTIPLE_CHOICE",
-      answerExplanationImages: [],
-      answers: [],
+    id,
+    content: "",
+    selectOptions: [],
+    answerExplanationContent: "",
+    answerType: "MULTIPLE_CHOICE",
+    answerExplanationImages: [],
+    answers: [],
   });
 
   const addQuizWriteForm = (id: number) => {
@@ -66,17 +88,26 @@ export default function QuizWriteForm() {
       {
         id: id,
         quizWriteFormType: "MULTIPLE_CHOICE", // TODO: BasicFormType으로 변수화
-        component: <QuizWriteFormItem id={id} deleteQuizWriteForm={deleteQuizWriteForm} quizWriteFormType={"MULTIPLE_CHOICE"}/>,
+        component: (
+          <QuizWriteFormItem
+            id={id}
+            deleteQuizWriteForm={deleteQuizWriteForm}
+            quizWriteFormType={"MULTIPLE_CHOICE"}
+          />
+        ),
       },
     ]);
-  }
+  };
 
   const onClickAddQuizWriteForm = () => {
     const id = Date.now();
-    const newQuestion : BookQuizQuestionType = createNewQuestion(id);
+    const newQuestion: BookQuizQuestionType = createNewQuestion(id);
     addQuizWriteForm(id);
-    updateQuizCreationInfo("questions",[...(quizCreationInfo.questions ?? []), newQuestion])
-    console.log("info: %o",quizCreationInfo);
+    updateQuizCreationInfo("questions", [
+      ...(quizCreationInfo.questions ?? []),
+      newQuestion,
+    ]);
+    console.log("info: %o", quizCreationInfo);
   };
 
   return (
@@ -84,9 +115,17 @@ export default function QuizWriteForm() {
       <DragDropContext onDragEnd={moveQuizWriteForm}>
         <Droppable droppableId="cardlists">
           {(provided) => (
-            <div className="cardlists" {...provided.droppableProps} ref={provided.innerRef}>
+            <div
+              className="cardlists"
+              {...provided.droppableProps}
+              ref={provided.innerRef}
+            >
               {quizWriteFormList.map((item, index) => (
-                <Draggable draggableId={`${item.id}`} index={index} key={`${item.id}`}>
+                <Draggable
+                  draggableId={`${item.id}`}
+                  index={index}
+                  key={`${item.id}`}
+                >
                   {(provided) => {
                     return (
                       <div
@@ -106,11 +145,20 @@ export default function QuizWriteForm() {
         </Droppable>
       </DragDropContext>
       <Button
-        size={"xlarge"}
+        size="large"
         onClick={onClickAddQuizWriteForm}
-        className={styles["add-quiz"]}>
-        <span>문제 추가하기</span>
-        <Plus stroke={primary} width={24} height={24} />
+        fullWidth
+        icon={
+          <QuizPlus
+            alt="스터디 그룹 추가 버튼"
+            width={20}
+            height={20}
+            stroke={primary}
+          />
+        }
+        color="secondary"
+      >
+        문제 추가하기
       </Button>
     </div>
   );

a16eb7c9fa359e4f9a1bc5f576caa392798b9b6f
hyerimRoh <mtxpvksh@gmail.com>
2024-11-20 00:38:34 +0900

    Refactor(DK-297): 순서 변경하는 함수 중복코드 제거

--- src/pages/CreateQuiz/composite/quizWriteForm/quizWriteForm.tsx
+++ src/pages/CreateQuiz/composite/quizWriteForm/quizWriteForm.tsx
@@ -52,21 +52,21 @@ export default function QuizWriteForm() {
   };
   const [questionForms, setQuestionForms] = useState<QuestionFormType[]>(setInitialForms());
 
+  const reorderItems = (items: any[], result: DropResult) => {
+    const reorderedItems = [...items];
+    const [movedItem] =  reorderedItems.splice(result.source.index, 1);
+    reorderedItems.splice(result.destination!.index, 0, movedItem);
+    return reorderedItems
+  }
+
   const moveQuestions = (result: DropResult) => {
     if (!result.destination) return;
-    const items = [...questionForms];
-    const [reorderedItem] = items.splice(result.source.index, 1);
-    items.splice(result.destination.index, 0, reorderedItem);
-    setQuestionForms(items);
 
-    //TODO: 위의 겹치는 로직과 리팩토링 필요
-    const globalQuizItems = [...(quizCreationInfo.questions ?? [])];
-    const [reorderedGlobalItem] = globalQuizItems.splice(
-      result.source.index,
-      1
-    );
-    globalQuizItems.splice(result.destination.index, 0, reorderedGlobalItem);
-    updateQuizCreationInfo("questions", globalQuizItems);
+    const updatedQuestionForms = reorderItems(questionForms, result);
+    setQuestionForms(updatedQuestionForms);
+
+    const updatedGlobalQuizItems = reorderItems(quizCreationInfo.questions ?? [], result);
+    updateQuizCreationInfo("questions", updatedGlobalQuizItems);
   };
 
   const createNewQuestion = (id: number): QuizQuestionType => ({

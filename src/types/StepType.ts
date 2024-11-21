efcf3ea1766a7465715d31f33f98105fd85feceb
hyerimRoh <mtxpvksh@gmail.com>
2024-11-21 11:37:09 +0900

    Refactor(DK-310):  Step type을 다른 폴더로 옮김

---
+++ src/types/StepType.ts
@@ -0,0 +1,16 @@
+import { Dispatch } from "react";
+import { SetStateAction } from "jotai";
+
+export interface FormComponentProps {
+    setCurrentStep: Dispatch<SetStateAction<number>>;
+  }
+  
+  export interface Step {
+    order?: number;
+    icon?: string;
+    title: string;
+    description?: string;
+    formComponent?: (props?: FormComponentProps) => JSX.Element;
+    subSteps?: Step[];
+    isDone?: boolean;
+  }

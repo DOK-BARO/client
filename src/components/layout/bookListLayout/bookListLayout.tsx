71bc00adb7321d14f4f7450bae95de3822acad01
page1597 <gimm08377@gmail.com>
2024-11-22 17:42:45 +0900

    Style(DK-311): 책 카테고리 브레드크럼 구현

--- src/components/layout/bookListLayout/bookListLayout.tsx
+++ src/components/layout/bookListLayout/bookListLayout.tsx
@@ -1,15 +1,43 @@
-import LNB from "../lnb/lnb";
 import { Outlet, useParams } from "react-router-dom";
 import styles from "./_book_list_layout.module.scss";
+import { useQuery } from "@tanstack/react-query";
+import { bookKeys } from "@/data/queryKeys";
+import { getBookCategories } from "@/services/server/bookService";
+import LNB from "../lnb/lnb";
+import {
+  findCurrentCategoryInfo,
+  findParentCategoryInfo,
+} from "@/utils/findCategoryInfo";
+import Breadcrumb from "../breadcrumb/breadcrumb";
 
 export default function BookListLayout() {
+  const { data: categories, isLoading } = useQuery({
+    queryKey: bookKeys.categories(),
+    queryFn: getBookCategories,
+  });
   const { categoryId } = useParams();
+
+  if (isLoading || !categories) {
+    return <div>로딩중</div>;
+  }
+
+  const parentCategoryInfo = findParentCategoryInfo(
+    categories,
+    Number(categoryId)
+  );
+  const currentCategoryInfo = findCurrentCategoryInfo(
+    categories,
+    Number(categoryId)
+  );
+
   return (
-    <div className={styles.container}>
-      <div className={styles["lnb-container"]}>
-        <LNB categoryId={Number(categoryId)} />
+    <section className={styles.container}>
+      <LNB categoryId={Number(categoryId)} categories={categories} />
+      <div className={styles["book-list-container"]}>
+        <h2 className={styles.title}>전체 책 목록</h2>
+        <Breadcrumb list={[parentCategoryInfo, currentCategoryInfo]} />
+        <Outlet />
       </div>
-      <Outlet />
-    </div>
+    </section>
   );
 }

2c39c089fecc631c107539e28db9e9f0d5d39191
hyerimRoh <mtxpvksh@gmail.com>
2024-11-25 15:51:29 +0900

    Feat(DK-310):마이페이지 progress bar 구현 -substep의 mainstep 호버 시 step 리스트 보이도록 구현 -substep 클릭 시 해당 step 리스트 보이도록 구현

--- src/pages/MyPage/layout/myPageSteps/myPageSteps.tsx
+++ src/pages/MyPage/layout/myPageSteps/myPageSteps.tsx
@@ -1,64 +1,85 @@
 import styles from "./_my_page_step.module.scss";
 import { Step } from "@/types/StepType";
 import Button from "@/components/atom/button/button";
+import { useState } from "react";
 
-export default function MyPageSteps({ steps, setCurrentStep }: {
+export default function MyPageSteps({ steps, currentStep, setCurrentStep }: {
   steps: Step[],
   currentStep: number;
   setCurrentStep: React.Dispatch<React.SetStateAction<number>>;
 }) {
+  const [activeSubStep, setActiveSubStep] = useState<number | null>(null);
+
   const onChangeStep = (e: React.MouseEvent<HTMLButtonElement>) => {
     const currentStepButtonValue = e.currentTarget.value;
     steps.forEach((step) => {
       if (step.title === currentStepButtonValue) {
         setCurrentStep(step.order!);
+        setActiveSubStep(null); // 메인 스텝 클릭 시 서브 스텝 비활성화
       }
       step.subSteps?.forEach((subStep) => {
         if (subStep.title === currentStepButtonValue) {
-          // TODO: 해당 섹션으로 이동
-          const sectionId: string = subStep.sectionId ?? "";
-          const sectionElement = document.getElementById(sectionId);
-
-          const headerOffset = 62;
-          const elementPosition = sectionElement!.getBoundingClientRect().top;
-          const offsetPosition = elementPosition + window.scrollY - headerOffset; // (viewport에서의 위치) + (스크롤한 위치) = 실제 element의 위치
-
-          window.scrollTo({
-            top: offsetPosition,
-            behavior: 'smooth'
-          });
+          setCurrentStep(subStep.order!);
+          setActiveSubStep(subStep.order); // 서브 스텝 클릭 시 해당 서브 스텝 활성화
         }
       });
     });
   }
   return (
     <aside className={styles["container"]}>
-      {steps.map((step, index: number) => (
-        <div key={index}>
-          <Button
-            onClick={(e) => onChangeStep(e)}
-            value={step.title}
-            className={styles["steps"]}
-            fullWidth
-            color="white"
-          >
-            {step.title}
-          </Button>
-          {step.subSteps &&
-            step.subSteps.map((subStep, index: number) => (
-              <Button key={index}
-                value={subStep.title}
+      <ul>
+        {steps.map((step, index: number) => {
+          const isActiveStep = currentStep === step.order;
+          const isAnySubStepActive = step.subSteps?.some(
+            (subStep) => subStep.order === currentStep
+          );
+          const firstSubStepOrder = step.subSteps?.[0]?.order;
+          const subStepsClass = activeSubStep === firstSubStepOrder || activeSubStep ? styles["is-visible"] : '';
+
+          return (
+            <li 
+            key={index}
+            className={
+              `${styles["steps"]}
+            ${styles[step.subSteps ? "have-sub-step" : ""]}
+            `
+            }
+            >
+              <Button
                 onClick={(e) => onChangeStep(e)}
-                className={styles["sub-steps"]}
-                color="transparent"
+                value={step.title}
                 fullWidth
+                color={
+                  isActiveStep || isAnySubStepActive ? "white" : "transparent"
+                }
               >
-                <div style={{ width: 20, height: 20 }} />
-                {subStep.title}
+                {step.title}
               </Button>
-            ))
-          }
-        </div>))}
+              <ul className={`${styles["sub-steps-container"]} ${subStepsClass}`}>
+                {step.subSteps &&
+                  step.subSteps.map((subStep, index: number) => (
+                    <li>
+                      <Button key={index}
+                        value={subStep.title}
+                        onClick={(e) => onChangeStep(e)}
+                        className={styles["sub-steps"]}
+                        color={
+                          currentStep === subStep.order
+                            ? "white"
+                            : "transparent"
+                        }
+                        fullWidth
+                      >
+                        <div style={{ width: 20, height: 20 }} />
+                        {subStep.title}
+                      </Button>
+                    </li>
+                  ))
+                }
+              </ul>
+            </li>)
+        })}
+      </ul>
     </aside>
   );
 }

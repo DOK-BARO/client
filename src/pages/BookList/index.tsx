4db8c0d17c945b4cbaedd3339cf4e237f0871290
hyerimRoh <mtxpvksh@gmail.com>
2024-11-26 17:11:51 +0900

    Fix(develop): 젠킨스 빌드를 위한 안쓰는 코드 제거

--- src/pages/BookList/index.tsx
+++ src/pages/BookList/index.tsx
@@ -1,5 +1,5 @@
-import React from "react";
-import styles from "./_book_list.module.scss";
+// import React from "react";
+// import styles from "./_book_list.module.scss";
 
 export default function Index() {
   return <div>Index</div>;

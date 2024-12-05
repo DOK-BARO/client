4e3c7455792ef39de68705f4809fd4ce544f2294
hyerimRoh <mtxpvksh@gmail.com>
2024-12-05 11:25:14 +0900

    Feat(DK-336): service파일 에러처리 로직에서 필요없는 코드 제거 - axios interceptor 적용으로 필요없어진 코드 제거

--- src/services/server/bookService.ts
+++ src/services/server/bookService.ts
@@ -5,8 +5,6 @@ import {
 } from "../../types/BookType.ts";
 import { BookDetailType } from "../../types/BookDetailType.ts";
 import { BookCategory } from "../../types/GNBCategoryType.ts";
-import axios, { AxiosError } from "axios";
-import localApi from "../local/LocalApi.ts";
 import { axiosInstance } from "@/config/axiosConfig.ts";
 
 // 책 목록, 책 상세정보 가져오기
@@ -40,14 +38,7 @@ class BookService {
       });
       console.log("책 목록!!", response);
       return response.data;
-    } catch (error: unknown) {
-      if (axios.isAxiosError(error)) {
-        const axiosError = error as AxiosError;
-        if (axiosError.response?.status === 403) {
-          localApi.removeCertification();
-          throw error;
-        }
-      }
+    } catch (error) {
       throw new Error(`책 목록 가져오기 실패: ${error}`);
     }
   };
@@ -64,14 +55,7 @@ class BookService {
         },
       });
       return data;
-    } catch (error: unknown) {
-      if (axios.isAxiosError(error)) {
-        const axiosError = error as AxiosError;
-        if (axiosError.response?.status === 403) {
-          localApi.removeCertification();
-          throw error;
-        }
-      }
+    } catch (error) {
       throw new Error(`책 통합검색 실패: ${error}`);
     }
   };
@@ -80,14 +64,7 @@ class BookService {
     try {
       const { data } = await axiosInstance.get(`/books/${bookId}`);
       return data;
-    } catch (error: unknown) {
-      if (axios.isAxiosError(error)) {
-        const axiosError = error as AxiosError;
-        if (axiosError.response?.status === 403) {
-          localApi.removeCertification();
-          throw error;
-        }
-      }
+    } catch (error) {
       throw new Error(`책 상세 가져오기 실패: ${error}`);
     }
   };
@@ -96,15 +73,7 @@ class BookService {
     try {
       const { data } = await axiosInstance.get("/book-categories");
       return data.details[0].details;
-    } catch (error: unknown) {
-      if (axios.isAxiosError(error)) {
-        const axiosError = error as AxiosError;
-        if (axiosError.response?.status === 403) {
-          console.log("Sdfsdf");
-          localApi.removeCertification();
-          throw error;
-        }
-      }
+    } catch (error) {
       throw new Error(`책 카테고리 가져오기 실패: ${error}`);
     }
   };
--- src/services/server/imageService.ts
+++ src/services/server/imageService.ts
@@ -1,5 +1,4 @@
 import { axiosInstance } from "@/config/axiosConfig";
-import axios, { AxiosError } from "axios";
 
 // 이미지 업로드
 class ImageService {
@@ -21,15 +20,7 @@ class ImageService {
       console.log("data: %o", data);
       return data.url;
     } catch (error: unknown) {
-      if (axios.isAxiosError(error)) {
-        const axiosError = error as AxiosError;
-        if (axiosError.response?.status === 404) {
-          throw error;
-        }
-        throw new Error(`파일(이미지) 업로드 실패: ${error}`);
-      } else {
-        throw new Error(`Unexpected error: ${error}`);
-      }
+			throw new Error(`파일(이미지) 업로드 실패: ${error}`);
     }
   };
 }

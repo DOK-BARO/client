68da3e23be2d2b0c21759b249aa7fde1a4bd12d9
page1597 <gimm08377@gmail.com>
2024-12-08 15:43:00 +0900

    Refactor(DK-348): 이용약관 데이터 패칭 useQuery 사용으로 변경

--- src/data/queryKeys.ts
+++ src/data/queryKeys.ts
@@ -7,11 +7,16 @@ export const bookKeys = {
   search: (params?: SearchBooksParams) => ["bookSearch", params] as const,
 };
 
+export const authKeys = {
+  terms: () => ["terms"] as const,
+  termDetail: (id: number | null) => ["termDetail", id] as const,
+};
+
 export const userKeys = {
   user: () => ["user"] as const,
 };
 
 export const quizKeys = {
   myQuiz: () => ["myQuiz"] as const,
-	detail: (id:string) => ["quizDetail",id] as const,
+  detail: (id: string) => ["quizDetail", id] as const,
 };

f576057abcd1f72e263b3f8d6c784e40c649328e
hyerimRoh <mtxpvksh@gmail.com>
2024-12-13 11:35:41 +0900

    Fix(DK-176): 책 상세페이지 디자인 변경사항 적용 - 페이지 상단의 책 정보섹션 구현

--- src/pages/BookDetail/index.tsx
+++ src/pages/BookDetail/index.tsx
@@ -2,30 +2,36 @@ import { useParams } from "react-router-dom";
 import styles from "./_book_detail.module.scss";
 import { useQuery } from "@tanstack/react-query";
 import { bookKeys } from "@/data/queryKeys.ts";
-import BookDetailSection from "./composite/bookDetailSection/bookDetailSection.tsx";
+import BookDetailContent from "./composite/bookDetailSection/bookDetailSection.tsx";
 import QuizListSection from "./composite/quizListSection/quizListSection.tsx";
 import { bookService } from "@/services/server/bookService.ts";
+import Breadcrumb from "@/components/composite/breadcrumb/breadcrumb.tsx";
 
 export default function Index() {
-  const { id } = useParams();
+	const { id } = useParams();
 
-  const { data, isLoading } = useQuery({
-    queryKey: bookKeys.detail(id!),
-    queryFn: () => bookService.fetchBook(id!),
-  });
+	const { data, isLoading } = useQuery({
+		queryKey: bookKeys.detail(id!),
+		queryFn: () => bookService.fetchBook(id!),
+	});
 
-  if (isLoading) {
-    return <div>loading</div>;
-  }
+	if (isLoading) {
+		return <div>loading</div>;
+	}
 
-  if (!data) {
-    return <div>book detail page error!!</div>;
-  }
+	if (!data) {
+		return <div>book detail page error!!</div>;
+	}
 
-  return (
-    <section className={styles.container}>
-      <BookDetailSection bookDetailContent={data} />
-      <QuizListSection />
-    </section>
-  );
+	return (
+		<section className={styles.container}>
+			<div className={styles["bread-crumb"]}>
+			<Breadcrumb list={data.categories.map((e, index) => ({ id: index, name: e.name }))} />
+			</div>
+			<div className={styles["book-detail-section"]}>
+				<BookDetailContent bookDetailContent={data} />
+				<QuizListSection />
+			</div>
+		</section>
+	);
 }

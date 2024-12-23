d3a59e3ec36af8ebe93ca973d028a3deca487cf7
page1597 <gimm08377@gmail.com>
2024-12-23 15:34:28 +0900

    Refactor(DK-400): 파라미터 처리 로직 개선
    - parseQueryParams 함수의 반환 로직을 조건부로 수정하여 category, page, size, quizId, direction, sort 등의 파라미터가 존재할 경우에만 포함되도록 변경

--- src/utils/parseQueryParams.ts
+++ src/utils/parseQueryParams.ts
@@ -17,5 +17,33 @@ export const parseQueryParams = <SortType>({
   sort,
   quizId,
 }: ParseQueryParamsArgs) => {
-  const params: { [key: string]: unknown } = {};
-ㅊ
+  const params: { [key: string]: any } = {};
+
+  if (category) {
+    params.category = Number(category); // category가 있을 경우에만 추가
+  }
+
+  if (page) {
+    params.page = Number(page);
+  } else {
+    params.page = 1; // 기본값 1
+  }
+
+  if (size) {
+    params.size = size;
+  }
+
+  if (quizId !== undefined) {
+    params.quizId = quizId;
+  }
+
+  if (direction) {
+    params.direction = direction as FilterType<SortType>["direction"];
+  }
+
+  if (sort) {
+    params.sort = sort as FilterType<SortType>["sort"];
+  }
+
+  return params;
+};

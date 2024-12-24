114f860e1da9a55aed8cb53050afa7346035cabe
hyerimRoh <mtxpvksh@gmail.com>
2024-12-24 15:46:36 +0900

    Fix(DK-176): 누락된 코드 추가

--- src/App.tsx
+++ src/App.tsx
@@ -28,114 +28,108 @@ import QuizResult from "./pages/QuizResult/index.tsx";
 import QuizReview from "./pages/QuizReview/index.tsx";
 
 function App() {
-  // TODO: 분리하기
-  const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <BaseLayout />,
-      children: [
-        {
-          path: "/",
-          element: <BookListLayout />,
-          children: [
-            {
-              path: "/",
-              element: <Navigate to="/books" replace />, // "/"로 들어왔을 때 "/books"로 리다이렉트
-            },
-            {
-              path: "/books",
-              element: <BookList />,
-            },
-          ],
-        },
-        {
-          path: "/quiz/:id",
-          element: <QuizDetail />,
-        },
-        {
-          path: "/create-quiz",
-          element: <CreateQuiz />,
-        },
-
-        {
-          path: "/create-quiz/complete",
-          element: <CreateQuizComplete />,
-        },
-        {
-          path: "/book/:id",
-          element: <BookDetailSection />,
-        },
-        {
-          path: "/find-password",
-          element: <FindPassword />,
-        },
-
-        {
-          path: "/register/:method",
-          element: <Register />,
-        },
-
-        {
-          path: "/register/complete",
-          element: <RegisterComplete />,
-        },
-        {
-          path: "/my",
-          element: <MyPage />,
-        },
-
-        {
-          // 공용 컴포넌트 미리보기를 위한 페이지
-          path: "/component-test",
-          element: <ComponentTest />,
-        },
-      ],
-    },
-    // {
-    //   path: "/oauth2/redirected/:provider",
-    //   element: <AuthRedirectedPage />,
-    // },
-    {
-      path: "/quiz/play/:quizId",
-      element: <NoHeaderLayout />,
-      children: [
-        {
-          path: "/quiz/play/:quizId",
-          element: <SolvingQuizPage />,
-        },
-      ],
-    },
+	// TODO: 분리하기
+	const router = createBrowserRouter([
 		{
-      path: "/quiz",
-      element: <NoHeaderLayout />,
-      children: [
+			path: "/",
+			element: <BaseLayout />,
+			children: [
 				{
-					path: "/quiz/result",
+					path: "/",
+					element: <BookListLayout />,
+					children: [
+						{
+							path: "/",
+							element: <Navigate to="/books" replace />, // "/"로 들어왔을 때 "/books"로 리다이렉트
+						},
+						{
+							path: "/books",
+							element: <BookList />,
+						},
+					],
+				},
+				{
+					path: "/quiz/:id",
+					element: <QuizDetail />,
+				},
+				{
+					path: "/create-quiz",
+					element: <CreateQuiz />,
+				},
+
+				{
+					path: "/create-quiz/complete",
+					element: <CreateQuizComplete />,
+				},
+				{
+					path: "/book/:id",
+					element: <BookDetailSection />,
+				},
+				{
+					path: "/find-password",
+					element: <FindPassword />,
+				},
+
+				{
+					path: "/register/:method",
+					element: <Register />,
+				},
+
+				{
+					path: "/register/complete",
+					element: <RegisterComplete />,
+				},
+				{
+					path: "/my",
+					element: <MyPage />,
+				},
+
+				{
+					// 공용 컴포넌트 미리보기를 위한 페이지
+					path: "/component-test",
+					element: <ComponentTest />,
+				},
+			],
+		},
+		// {
+		//   path: "/oauth2/redirected/:provider",
+		//   element: <AuthRedirectedPage />,
+		// },
+		{
+			path: "/quiz",
+			element: <NoHeaderLayout />,
+			children: [
+				{
+					path: "/quiz/play/:quizId",
+					element: <SolvingQuizPage />,
+				},
+				{
+					path: "/quiz/result/:quizId/:solvingQuizId/:quizTitle",
 					element: <QuizResult />
 				},
 				{
-					path: "/quiz/review",
+					path: "/quiz/review/:quizId/:solvingQuizId/:quizTitle",
 					element: <QuizReview />
 				}
-      ],
-    },
-    {
-      path: "*",
-      element: <NoHeaderLayout />,
-      children: [
-        {
-          path: "*",
-          element: <NotFound />,
-        },
-      ],
-    },
-  ]);
-  return (
-    <QueryClientProvider client={queryClient}>
-      <RouterProvider router={router} />
-      <ToastPortal />
-    </QueryClientProvider>
-  );
+			],
+		},
+		{
+			path: "*",
+			element: <NoHeaderLayout />,
+			children: [
+				{
+					path: "*",
+					element: <NotFound />,
+				},
+			],
+		},
+	]);
+	return (
+		<QueryClientProvider client={queryClient}>
+			<RouterProvider router={router} />
+			<ToastPortal />
+		</QueryClientProvider>
+	);
 }
 
 export default App;

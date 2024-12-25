7f81e1463fd467b9eb079dcd71534c05daa77dc2
page1597 <gimm08377@gmail.com>
2024-12-25 17:24:22 +0900

    Refactor(DK-406): 마이페이지 컴포넌트 단일 관리에서 하위 라우트 렌더링 방식으로 전환

--- src/types/PaginationType.ts
+++ src/types/PaginationType.ts
@@ -1,5 +1,9 @@
 export type PagePositionType = "START" | "END" | "BETWEEN";
-export type ParentComponentType = "BOOKS" | "MY" | "QUIZ" | `BOOK/${string}`;
+export type ParentComponentType =
+  | "BOOKS"
+  | `MY/${string}`
+  | "QUIZ"
+  | `BOOK/${string}`;
 export interface PaginationType {
   parentComponentType: ParentComponentType; // 페이지 타입: ex) 책 목록, 마이페이지
   currentPage: number;

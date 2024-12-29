561ead24691fd0a5be4f48db44beee629e46ed84
page1597 <gimm08377@gmail.com>
2024-12-30 01:00:24 +0900

    Refactor, Feat: 마이페이지 네비게이션 사이드 컴포넌트 동작 수정 (코드 복구)
    - 덮어씌워진 코드 복구

--- src/pages/MyPage/index.tsx
+++ src/pages/MyPage/index.tsx
@@ -1,65 +1,37 @@
 import styles from "./_mypage.module.scss";
-import { Step } from "@/types/StepType";
-import MyPageSteps from "./layout/MyPageSteps/MyPageSteps";
 import MyPageLayout from "./layout/MyPageLayout/MyPageLayout";
-import { useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { SectionNavType } from "@/types/SectionNavType";
+import MyPageSectionNav from "./layout/MyPageSectionNav/MyPageSectionNav";
 
 export default function Index() {
-  const [currentStep, setCurrentStep] = useState<number>(0);
-  const navigate = useNavigate();
-
-  const getCurrentStep = (): Step => {
-    const step = steps[currentStep];
-    if (step) return step;
-
-    const truncatedStep = Math.trunc(currentStep);
-    return steps[truncatedStep]!.subSteps!.find(
-      (subStep) => subStep.order === currentStep
-    )!;
-  };
-
-  // 페이지 이동
-  useEffect(() => {
-    const step: Step = getCurrentStep();
-    navigate(`/my/${step.pageLink}`);
-  }, [currentStep]);
-
-  const steps: Step[] = [
+  const sectionNavList: SectionNavType[] = [
     {
-      order: 0,
       title: "만든 퀴즈",
-      pageLink: "made-quiz",
+      link: "made-quiz",
     },
     {
-      order: 1,
       title: "푼 퀴즈",
-      pageLink: "solved-quiz",
+      link: "solved-quiz",
     },
     {
-      order: 2,
       title: "내 스터디 그룹",
-      pageLink: "study-groups",
+      link: "study-groups",
     },
     {
-      order: 3,
       title: "계정 설정",
-      pageLink: "settings",
-      subSteps: [
+      link: "settings",
+      subTitles: [
         {
-          order: 3.1,
           title: "회원정보 수정",
-          pageLink: "settings/edit-profile",
+          link: "settings/edit-profile",
         },
         {
-          order: 3.2,
           title: "비밀번호 변경",
-          pageLink: "settings/change-password",
+          link: "settings/change-password",
         },
         {
-          order: 3.3,
           title: "회원 탈퇴",
-          pageLink: "settings/delete-account",
+          link: "settings/delete-account",
         },
       ],
     },
@@ -68,11 +40,7 @@ export default function Index() {
   return (
     <section className={styles["container"]}>
       <div className={styles["section-content"]}>
-        <MyPageSteps
-          steps={steps}
-          currentStep={currentStep}
-          setCurrentStep={setCurrentStep}
-        />
+        <MyPageSectionNav sectionNavList={sectionNavList} />
         <MyPageLayout />
       </div>
     </section>

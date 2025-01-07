fbbc9d9c11e987131c532e445e2481ca467845db
hyerimRoh <mtxpvksh@gmail.com>
2025-01-07 15:08:01 +0900

    Fix(DK-420): 공백 문제 확인 중 - convert indentation to space 켬

--- src/pages/Home/components/composite/BookItem/BookItem.tsx
+++ src/pages/Home/components/composite/BookItem/BookItem.tsx
@@ -5,34 +5,34 @@ import pencil from "/assets/svg/bookList/pencil.svg";
 import ROUTES from "@/data/routes";
 
 export default function BookItem({ book }: { book: BookType }) {
-	const navigate = useNavigate();
-	const handleClick = () => {
-		navigate(ROUTES.BOOK_DETAIL_SECTION(book.id));
-	};
-	const bookAuthor =
-		book.authors.length > 1 ? `${book.authors[0]} 외` : book.authors;
+  const navigate = useNavigate();
+  const handleClick = () => {
+    navigate(ROUTES.BOOK_DETAIL_SECTION(book.id));
+  };
+  const bookAuthor =
+    book.authors.length > 1 ? `${book.authors[0]} 외` : book.authors;
 
-	return (
-		<article className={styles["book-item"]} onClick={handleClick}>
-			<figure>
-				<img className={styles.img} src={book.imageUrl} alt={book.title} />
-				<figcaption>{book.title}</figcaption>
-			</figure>
-			<div className={styles["book-item-info"]}>
-				<p className={styles.title} aria-label="책 제목">
-					{book.title}
-				</p>
-				<p className={styles.authors} aria-label="저자">
-					{bookAuthor}
-				</p>
-				<p className={styles.publisher} aria-label="출판사">
-					{book.publisher}
-				</p>
-				<span className={styles["quiz-count-container"]}>
-					<img src={pencil} alt="퀴즈 개수" width={20} height={20} />
-					<p className={styles["quiz-count"]}>{book.quizCount}</p>
-				</span>
-			</div>
-		</article>
-	);
+  return (
+    <article className={styles["book-item"]} onClick={handleClick}>
+      <figure>
+        <img className={styles.img} src={book.imageUrl} alt={book.title} />
+        <figcaption>{book.title}</figcaption>
+      </figure>
+      <div className={styles["book-item-info"]}>
+        <p className={styles.title} aria-label="책 제목">
+          {book.title}
+        </p>
+        <p className={styles.authors} aria-label="저자">
+          {bookAuthor}
+        </p>
+        <p className={styles.publisher} aria-label="출판사">
+          {book.publisher}
+        </p>
+        <span className={styles["quiz-count-container"]}>
+          <img src={pencil} alt="퀴즈 개수" width={20} height={20} />
+          <p className={styles["quiz-count"]}>{book.quizCount}</p>
+        </span>
+      </div>
+    </article>
+  );
 }

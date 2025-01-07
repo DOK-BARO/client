44afc76b91b462fd79464b06509e319e7800ccf2
page1597 <gimm08377@gmail.com>
2025-01-07 23:17:50 +0900

    Refactor(DK-415): MyPageSectionNav 중복 로직 제거 및 리팩토링
    - activeLink 계산 로직을 함수로 분리하여 useEffect와 handleSectionChange에서 재사용
    - Navbar 클릭 시 링크 업데이트 및 navigate
    - pathname에 따른 (의존성 배열) activeLink 상태 초기화

--- src/pages/MyPage/layout/MyPageSectionNav/MyPageSectionNav.tsx
+++ src/pages/MyPage/layout/MyPageSectionNav/MyPageSectionNav.tsx
@@ -8,94 +8,103 @@ import { useAtom } from "jotai";
 import { currentUserAtom } from "@/store/userAtom";
 
 interface Prop {
-	sectionNavList: SectionNavType[];
+  sectionNavList: SectionNavType[];
 }
 
+const calculateActiveLink = (pathname: string): string | null => {
+  const paths = pathname.split("/");
+  const basePath = paths[1] === "my" ? paths[2] : null;
+  const subPath = paths[3];
+
+  return subPath === undefined || isNumberString(subPath)
+    ? basePath || ""
+    : `${basePath}/${subPath}`;
+};
+
 export default function MyPageSectionNav({ sectionNavList }: Prop) {
-	const navigate = useNavigate();
-	const location = useLocation();
-	const [currentUser] = useAtom(currentUserAtom);
-	const [activeLink, setActiveLink] = useState<string | null>(null);
-	const [isSubNavItemClicked, setIsSubNavItemClicked] =
-		useState<boolean>(false);
+  const navigate = useNavigate();
+  const location = useLocation();
+  const [currentUser] = useAtom(currentUserAtom);
+  const [activeLink, setActiveLink] = useState<string | null>(null);
+  const [isSubNavItemClicked, setIsSubNavItemClicked] =
+    useState<boolean>(false);
 
-	useEffect(() => {
-		const paths = location.pathname.split("/");
-		const basePath = paths[1] === "my" ? paths[2] : null;
-		const subPath = paths[3];
+  useEffect(() => {
+    setActiveLink(calculateActiveLink(location.pathname));
+  }, [location.pathname]);
 
-		if (subPath === undefined || isNumberString(subPath)) {
-			setActiveLink(basePath);
-		} else {
-			setActiveLink(`${basePath}/${subPath}`);
-		}
-	}, [location.pathname]);
+  // activeLink 업데이트 및 navigate
+  const handleSectionChange = (
+    e: React.MouseEvent<HTMLButtonElement>,
+    isSubNavItem: boolean = false
+  ) => {
+    const { value } = e.currentTarget;
+    setActiveLink(calculateActiveLink(value));
+    navigate(value);
+    setIsSubNavItemClicked(isSubNavItem);
+  };
 
-	const handleSectionChange = (
-		e: React.MouseEvent<HTMLButtonElement>,
-		isSubNavItem: boolean = false
-	) => {
-		const { value } = e.currentTarget;
-		setActiveLink(value);
-		navigate(value);
-		setIsSubNavItemClicked(isSubNavItem);
-	};
-
-	return (
-		<aside className={styles["container"]}>
-			<ul>
-				{sectionNavList.map((sectionNavItem, index: number) => {
-					return (
-						<li
-							key={index}
-							className={`${styles["steps"]} ${sectionNavItem.subTitles ? styles["have-sub-step"] : ""
-								}`}
-						>
-							<Button
-								onClick={handleSectionChange}
-								value={sectionNavItem.link}
-								fullWidth
-								color={
-									activeLink === sectionNavItem.link ? "white" : "transparent"
-								} // 클릭된 버튼만 색상 변경
-							>
-								{sectionNavItem.title}
-							</Button>
-							<ul
-								className={`${styles["sub-steps"]} ${isSubNavItemClicked ? styles["is-visible"] : ""
-									}}`}
-							>
-								{sectionNavItem.subTitles &&
-									sectionNavItem.subTitles.map((subNavItem, index: number) => {
-										const isEmailAuthType: boolean = (currentUser?.accountType === "EMAIL");
-										const isCurrentTitlePassword: boolean = subNavItem.title === "비밀번호 변경";
-										console.log(`${sectionNavItem.title}1111"`);
-										if ((!isEmailAuthType) && isCurrentTitlePassword) {
-											console.log(`${isEmailAuthType}!!!"`);
-											return (<div></div>);
-										}
-										return (<li key={index}>
-											<Button
-												value={subNavItem.link}
-												onClick={(e) => handleSectionChange(e, true)}
-												className={styles["sub-steps"]}
-												color={
-													activeLink === subNavItem.link
-														? "white"
-														: "transparent"
-												} // 서브 버튼 색상 변경
-												fullWidth
-											>
-												<div style={{ width: 20, height: 20 }} />
-												{subNavItem.title}
-											</Button>
-										</li>)
-									})}
-							</ul>
-						</li>
-					);
-				})}
-			</ul>
-		</aside>
-	);
+  return (
+    <aside className={styles["container"]}>
+      <ul>
+        {sectionNavList.map((sectionNavItem, index: number) => {
+          return (
+            <li
+              key={index}
+              className={`${styles["steps"]} ${
+                sectionNavItem.subTitles ? styles["have-sub-step"] : ""
+              }`}
+            >
+              <Button
+                onClick={handleSectionChange}
+                value={sectionNavItem.link}
+                fullWidth
+                color={
+                  activeLink === sectionNavItem.link ? "white" : "transparent"
+                } // 클릭된 버튼만 색상 변경
+              >
+                {sectionNavItem.title}
+              </Button>
+              <ul
+                className={`${styles["sub-steps"]} ${
+                  isSubNavItemClicked ? styles["is-visible"] : ""
+                }}`}
+              >
+                {sectionNavItem.subTitles &&
+                  sectionNavItem.subTitles.map((subNavItem, index: number) => {
+                    const isEmailAuthType: boolean =
+                      currentUser?.accountType === "EMAIL";
+                    const isCurrentTitlePassword: boolean =
+                      subNavItem.title === "비밀번호 변경";
+                    console.log(`${sectionNavItem.title}1111"`);
+                    if (!isEmailAuthType && isCurrentTitlePassword) {
+                      console.log(`${isEmailAuthType}!!!"`);
+                      return <div></div>;
+                    }
+                    return (
+                      <li key={index}>
+                        <Button
+                          value={subNavItem.link}
+                          onClick={(e) => handleSectionChange(e, true)}
+                          className={styles["sub-steps"]}
+                          color={
+                            activeLink === subNavItem.link
+                              ? "white"
+                              : "transparent"
+                          } // 서브 버튼 색상 변경
+                          fullWidth
+                        >
+                          <div style={{ width: 20, height: 20 }} />
+                          {subNavItem.title}
+                        </Button>
+                      </li>
+                    );
+                  })}
+              </ul>
+            </li>
+          );
+        })}
+      </ul>
+    </aside>
+  );
 }

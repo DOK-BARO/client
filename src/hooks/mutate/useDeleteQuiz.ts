642f2de4ce8b8b32827891282305e90bda3be3a0
hyerimRoh <mtxpvksh@gmail.com>
2025-01-09 17:14:15 +0900

    Chore(DK-362): indentation 수정

--- src/hooks/mutate/useDeleteQuiz.ts
+++ src/hooks/mutate/useDeleteQuiz.ts
@@ -5,16 +5,16 @@ import toast from "react-hot-toast";
 import { useNavigate } from "react-router-dom";
 
 const useDeleteQuiz = () => {
-	const navigate = useNavigate();
-	const { mutate: deleteQuiz } = useMutation<void, ErrorType, string>({
-		mutationFn: (quizId: string) => quizService.deleteQuiz(quizId),
-		onSuccess: () => {
-			toast.success("퀴즈가 삭제되었습니다.");
-			navigate(0);
-		}
-	});
+  const navigate = useNavigate();
+  const { mutate: deleteQuiz } = useMutation<void, ErrorType, string>({
+    mutationFn: (quizId: string) => quizService.deleteQuiz(quizId),
+    onSuccess: () => {
+      toast.success("퀴즈가 삭제되었습니다.");
+      navigate(0);
+    }
+  });
 
-	return { deleteQuiz };
+  return { deleteQuiz };
 }
 
 export default useDeleteQuiz;

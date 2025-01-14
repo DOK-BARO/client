308d62e35c1bb17cc4b0f267fff1959ddfe63b6f
page1597 <gimm08377@gmail.com>
2025-01-14 15:34:40 +0900

    Fix(DK-451): 책 상세 페이지에서 GNB 클릭으로 이동 시 필터 초기화되도록 수정
    - useFIlter.ts

--- src/components/atom/HeaderLogo/HeaderLogo.tsx
+++ src/components/atom/HeaderLogo/HeaderLogo.tsx
@@ -2,20 +2,31 @@ import styles from "./_header_logo.module.scss";
 import React from "react";
 import { APP_NAME } from "@/data/constants.ts";
 import { Logo } from "@/svg/Logo";
-import { Link } from "react-router-dom";
+import Button from "../Button/Button";
+import { useNavigate } from "react-router-dom";
 
 const HeaderLogo: React.FC = () => {
+  const navigate = useNavigate();
+  const handleNavigateToMain = () => {
+    navigate("/");
+  };
   return (
     <div className={styles["logo-container"]}>
-      <Link to="/" className={styles["logo-link"]}>
-        <Logo
-          width={43}
-          height={43}
-          alt="로고 버튼"
-          className={styles["logo-icon"]}
-        />
+      <Button
+        className={styles["logo-link"]}
+        iconPosition="left"
+        onClick={handleNavigateToMain}
+        icon={
+          <Logo
+            width={43}
+            height={43}
+            alt="로고 버튼"
+            className={styles["logo-icon"]}
+          />
+        }
+      >
         <h1>{APP_NAME}</h1>
-      </Link>
+      </Button>
     </div>
   );
 };

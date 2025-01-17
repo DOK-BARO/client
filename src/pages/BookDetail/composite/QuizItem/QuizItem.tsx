c04a674d5be9034e551380826336d64623318f05
page1597 <gimm08377@gmail.com>
2025-01-17 16:45:33 +0900

    Fix(DK-451): 책 상세 퀴즈 클릭 시 동작 이상 수정
    - a 태그 제거 및 e.stopPropagation(); 추가하여 이벤트 버블링 차단

--- src/pages/BookDetail/composite/QuizItem/QuizItem.tsx
+++ src/pages/BookDetail/composite/QuizItem/QuizItem.tsx
@@ -6,60 +6,73 @@ import { BookQuizzesDataType } from "@/types/BookType";
 import { useNavigate } from "react-router-dom";
 import ROUTES from "@/data/routes";
 
-export default function QuizItem({ quiz }: { quiz: BookQuizzesDataType }) {
-	const navigate = useNavigate();
+interface Props {
+  quiz: BookQuizzesDataType;
+  onClick: () => void;
+}
 
-	const goToPlayQuiz = async (e: React.MouseEvent<HTMLButtonElement>) => {
-		e.preventDefault();
-		navigate(ROUTES.SOLVING_QUIZ(quiz.id));
-	}
+export default function QuizItem({ quiz, onClick }: Props) {
+  const navigate = useNavigate();
 
-	const renderQuizDifficultyLevel = (): string => {
-		if (quiz.averageDifficultyLevel === 0) {
-			return "쉬움";
-		} else if (quiz.averageDifficultyLevel === 1) {
-			return "보통";
-		} else {
-			return "어려움";
-		}
-	}
-	return (
-		<div className={styles["container"]}>
-			<div className={styles["content"]}>
-				<div className={styles["header"]}>
-					<div className={styles["review"]}>
-						<Star className={styles["review-icon"]} />
-						<span>{quiz.averageStarRating}</span>
-						<span>{`/5 (${quiz.reviewCount}개의 후기)`}</span>
-					</div>
+  const goToPlayQuiz = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    e.stopPropagation();
+    navigate(ROUTES.SOLVING_QUIZ(quiz.id));
+  };
 
-					<div className={styles["quiz-level"]}>
-						<QuizLevelBarChart level={Math.round(quiz.averageDifficultyLevel)} width={24} height={24} />
-						<span>{renderQuizDifficultyLevel()}</span>
-					</div>
-				</div>
+  const renderQuizDifficultyLevel = (): string => {
+    if (quiz.averageDifficultyLevel === 0) {
+      return "쉬움";
+    } else if (quiz.averageDifficultyLevel === 1) {
+      return "보통";
+    } else {
+      return "어려움";
+    }
+  };
 
-				<div className={styles["quiz-title"]}>{quiz.title}</div>
-				<Button
-					className={styles['quiz-tag']}
-					size="xsmall" color="secondary">{quiz.questionCount} 문제</Button>
-			</div>
-			<div className={styles["footer"]}>
-				<div className={styles["study-group-info"]}>
-					<img
-						className={styles["study-group-img"]}
-						src={quiz.creator.profileUrl}
-						alt={"스터디 그룹 이미지"}
-					/>
-					<span>{quiz.creator.nickname}</span>
-				</div>
-				<Button
-					size="small"
-					color="primary"
-					className={styles["take-quiz-button"]} onClick={goToPlayQuiz}>
-					풀기
-				</Button>
-			</div>
-		</div>
-	);
+  return (
+    <div className={styles["container"]} onClick={onClick}>
+      <div className={styles["content"]}>
+        <div className={styles["header"]}>
+          <div className={styles["review"]}>
+            <Star className={styles["review-icon"]} />
+            <span>{quiz.averageStarRating}</span>
+            <span>{`/5 (${quiz.reviewCount}개의 후기)`}</span>
+          </div>
+
+          <div className={styles["quiz-level"]}>
+            <QuizLevelBarChart
+              level={Math.round(quiz.averageDifficultyLevel)}
+              width={24}
+              height={24}
+            />
+            <span>{renderQuizDifficultyLevel()}</span>
+          </div>
+        </div>
+
+        <div className={styles["quiz-title"]}>{quiz.title}</div>
+        <Button className={styles["quiz-tag"]} size="xsmall" color="secondary">
+          {quiz.questionCount} 문제
+        </Button>
+      </div>
+      <div className={styles["footer"]}>
+        <div className={styles["study-group-info"]}>
+          <img
+            className={styles["study-group-img"]}
+            src={quiz.creator.profileUrl}
+            alt={"스터디 그룹 이미지"}
+          />
+          <span>{quiz.creator.nickname}</span>
+        </div>
+        <Button
+          size="small"
+          color="primary"
+          className={styles["take-quiz-button"]}
+          onClick={goToPlayQuiz}
+        >
+          풀기
+        </Button>
+      </div>
+    </div>
+  );
 }

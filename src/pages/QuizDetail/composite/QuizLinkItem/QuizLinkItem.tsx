77b715e7e36b469839abb4dbb205a6f95e0b14e7
page1597 <gimm08377@gmail.com>
2025-01-24 17:37:06 +0900

    Feat(Dk-483): 퀴즈 상세에서 책 클릭 시 책 상세 페이지로 이동

--- src/pages/QuizDetail/composite/QuizLinkItem/QuizLinkItem.tsx
+++ src/pages/QuizDetail/composite/QuizLinkItem/QuizLinkItem.tsx
@@ -2,7 +2,7 @@ import Button from "@/components/atom/Button/Button";
 import styles from "./_quiz_link_item.module.scss";
 import { QuizExplanationType } from "@/types/QuizType";
 import { formatDate } from "@/utils/formatDate";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import ROUTES from "@/data/routes";
 
 interface Props {
@@ -16,13 +16,15 @@ export default function QuizLinkItem({ quizExplanation }: Props) {
   };
   return (
     <article className={styles.container}>
-      <img
-        className={styles["book-image"]}
-        src={quizExplanation.book.imageUrl}
-        alt={quizExplanation.book.title}
-        width={230}
-        height={304}
-      />
+      <Link to={`${ROUTES.BOOK_DETAIL_SECTION(quizExplanation.book.id)}`}>
+        <img
+          className={styles["book-image"]}
+          src={quizExplanation.book.imageUrl}
+          alt={quizExplanation.book.title}
+          width={230}
+          height={304}
+        />
+      </Link>
       <div className={styles["last-modified-at-container"]}>
         {/* TODO: 생성일 / 수정일 */}
         <p className={styles.label}>최종 수정일</p>

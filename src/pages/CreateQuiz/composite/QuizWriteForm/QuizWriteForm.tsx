b0433938721a30a29c5192e3ac5e32448330c0aa
page1597 <gimm08377@gmail.com>
2025-02-05 14:08:58 +0900

    Fix(DK-509): 퀴즈작성 "퀴즈 스텝 간 이동" 시 퀴즈 폼 데이터들이 초기화 되는 문제 수정

--- src/pages/CreateQuiz/composite/QuizWriteForm/QuizWriteForm.tsx
+++ src/pages/CreateQuiz/composite/QuizWriteForm/QuizWriteForm.tsx
@@ -10,7 +10,7 @@ import { AnswerType } from "@/types/QuizType";
 import React from "react";
 import { useAtom } from "jotai";
 import { QuizCreationType } from "@/types/QuizType";
-import { quizCreationInfoAtom } from "@/store/quizAtom";
+import { isFirstVisitAtom, quizCreationInfoAtom } from "@/store/quizAtom";
 import {
   closestCenter,
   DndContext,
@@ -117,16 +117,24 @@ const QuizWriteForm = React.memo(() => {
 
     const [questionForms, setQuestionForms] =
       useState<QuestionFormType[]>(setInitialForms());
+    const [isFirstVisit] = useAtom(isFirstVisitAtom);
 
     const [isInitialized, setIsInitialized] = useState<boolean>(false);
     useEffect(() => {
-      if (isInitialized) {
+      const isFirstVisitInLocalStorageState =
+        localStorage.getItem("firstVisit") !== "false";
+
+      if (isInitialized || !isFirstVisitInLocalStorageState) {
+        setIsInitialized(true);
         return;
       }
-      // 초기화
+
+      // 가이드라인 단계 완료(로컬 스토리지 첫 방문 여부가 아직 "true"일 경우) 후 퀴즈 질문폼 초기화
       setQuestionForms([]);
       setIsInitialized(true);
-    }, [quizCreationInfo, isInitialized]);
+      // 방문한 사용자로 표시
+      localStorage.setItem("firstVisit", "false");
+    }, [isFirstVisit]);
 
     const handleDragStart = (event: DragStartEvent) => {
       const { active } = event;
@@ -248,9 +256,7 @@ const QuizWriteForm = React.memo(() => {
           문제 추가하기
         </Button>
       </section>
-    ) : (
-      <div>됴잉</div>
-    );
+    ) : null;
   }
 });
 

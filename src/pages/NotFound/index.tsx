64834986d23026fe5bc6ade62ba3ee7dbf0c9dbb
page1597 <gimm08377@gmail.com>
2025-02-11 15:18:22 +0900

    Fix: 빌드 에러 수정

--- src/pages/NotFound/index.tsx
+++ src/pages/NotFound/index.tsx
@@ -5,16 +5,13 @@ import ROUTES from "@/data/routes";
 import errorImage from "/public/assets/image/404-error-image.png";
 
 export default function NotFound() {
-  // TODO: 수정
-  const errorImgPath = "/public/assets/svg/notFoundPage/404ErrorImage.svg";
-
   const navigate = useNavigate();
   const handleGoToHome = () => {
     navigate(ROUTES.ROOT);
   };
   return (
     <div className={styles["container"]}>
-      <img src={errorImage} alt="없는 페이지 입니다." />
+      <img src={errorImage} width={858} alt="없는 페이지 입니다." />
       <Button
         color="primary"
         size="medium"

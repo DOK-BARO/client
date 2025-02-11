f7f92483bf0f6114096e65fca811b6efb1c7abfa
page1597 <gimm08377@gmail.com>
2025-02-11 15:55:56 +0900

    Feat(DK-550): 초기 렌더링 시 텍스트 겹침 여부 파악

--- src/hooks/useTextOverlap.ts
+++ src/hooks/useTextOverlap.ts
@@ -1,32 +1,49 @@
-import { useState, useEffect, useRef } from "react";
+import { useState, useEffect, useRef, useLayoutEffect } from "react";
 
 const useTextOverlap = () => {
-  const [isOverlapping, setIsOverlapping] = useState(false);
   const textRef1 = useRef<HTMLDivElement | null>(null);
   const textRef2 = useRef<HTMLDivElement | null>(null);
+  const [isOverlapping, setIsOverlapping] = useState(false);
+
+  const checkOverlap = () => {
+    if (textRef1.current && textRef2.current) {
+      const rect1 = textRef1.current.getBoundingClientRect();
+      const rect2 = textRef2.current.getBoundingClientRect();
+
+      return !(
+        rect1.right < rect2.left ||
+        rect1.left > rect2.right ||
+        rect1.bottom < rect2.top ||
+        rect1.top > rect2.bottom
+      );
+    }
+    return false;
+  };
+
+  useLayoutEffect(() => {
+    const initialCheck = setTimeout(() => {
+      setIsOverlapping(checkOverlap());
+    }, 50);
+
+    return () => clearTimeout(initialCheck);
+  }, []);
 
   useEffect(() => {
-    const checkOverlap = () => {
-      if (textRef1.current && textRef2.current) {
-        const rect1 = textRef1.current.getBoundingClientRect();
-        const rect2 = textRef2.current.getBoundingClientRect();
+    let timeoutId: ReturnType<typeof setTimeout>;
 
-        const isOverlapping = !(
-          rect1.right < rect2.left ||
-          rect1.left > rect2.right ||
-          rect1.bottom < rect2.top ||
-          rect1.top > rect2.bottom
-        );
-
-        setIsOverlapping(isOverlapping);
-      }
+    const debouncedCheckOverlap = () => {
+      clearTimeout(timeoutId);
+      timeoutId = setTimeout(() => {
+        setIsOverlapping(checkOverlap());
+      }, 100);
     };
 
-    checkOverlap();
+    window.addEventListener("resize", debouncedCheckOverlap);
 
-    window.addEventListener("resize", checkOverlap);
-
-    return () => window.removeEventListener("resize", checkOverlap);
+    return () => {
+      window.removeEventListener("resize", debouncedCheckOverlap);
+      clearTimeout(timeoutId);
+    };
   }, []);
 
   return { isOverlapping, textRef1, textRef2 };

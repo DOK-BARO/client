18753d66c1e2b3adfbf1bea725c3851191162b27
hyerimRoh <mtxpvksh@gmail.com>
2025-02-11 10:51:33 +0900

    Fix(DK-532): 마크다운 보이도록 수정 - 퀴즈 제목, 퀴즈 옵션, 퀴즈 해설에서 마크다운 지원하도록 구현

--- src/components/atom/Checkbox/Checkbox.tsx
+++ src/components/atom/Checkbox/Checkbox.tsx
@@ -3,6 +3,9 @@ import styles from "./_checkbox.module.scss";
 import { Close } from "@/svg/Close";
 import { gray40, gray70 } from "@/styles/abstracts/colors";
 import Textarea from "../Textarea/Textarea";
+import ReactMarkdown from "react-markdown";
+import rehypeHighlight from "rehype-highlight";
+import "highlight.js/styles/xcode.css";
 import Button from "../Button/Button";
 import { useState } from "react";
 
@@ -112,7 +115,13 @@ export default function CheckBox({
           type === "solving-correct" ||
           type === "solving-incorrect" ||
           type === "checkbox-black") && (
-          <div className={styles["option-label-value"]}>{value}</div>
+          <div
+            className={`${styles["option-label-value"]} ${styles["markdown-content"]}`}
+          >
+            <ReactMarkdown rehypePlugins={[rehypeHighlight]}>
+              {value}
+            </ReactMarkdown>
+          </div>
         )}
         {(type === "checkbox-writing" ||
           type === "checkbox-correct" ||

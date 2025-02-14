503f634eb44fe95143810a851c5f3ddd8bef2937
hyerimRoh <mtxpvksh@gmail.com>
2025-02-14 14:38:28 +0900

    Refactor(DK-562): 헤더유틸 코드 리팩토링 - Button컴포넌트 사용 - 불필요한 코드 제거

--- src/components/composite/QuizMenuList/QuizMenuList.tsx
+++ src/components/composite/QuizMenuList/QuizMenuList.tsx
@@ -5,6 +5,7 @@ import checkSquare from "/public/assets/svg/header/check-square.svg";
 import ROUTES from "@/data/routes";
 import useLoginAction from "@/hooks/useLoginAction";
 import toast from "react-hot-toast";
+import Button from "@/components/atom/Button/Button";
 interface HeaderQuizUtilListProps {
   closeDropDownList: () => void;
 }
@@ -31,23 +32,25 @@ export default function HeaderQuizUtilList({
     navigate(ROUTES.BOOK_LIST);
   };
 
+  const headerQuizUtilList = [
+    { icon: pencil, title: "퀴즈 만들기", onClick: onClickMakeQuiz },
+    { icon: checkSquare, title: "퀴즈 풀기", onClick: onClickDoingQuiz },
+  ];
+
   return (
-    // TODO: 버튼으로 만들기
     <ul className={styles["header-quiz-util-list"]}>
-      <li
-        onClick={onClickMakeQuiz}
-        className={styles["header-quiz-util-list-item"]}
-      >
-        <img src={pencil} width={24} height={24} alt="" />
-        <span>퀴즈 만들기</span>
-      </li>
-      <li
-        onClick={onClickDoingQuiz}
-        className={styles["header-quiz-util-list-item"]}
-      >
-        <img src={checkSquare} width={24} height={24} />
-        <span>퀴즈 풀기</span>
-      </li>
+      {headerQuizUtilList.map((item) => (
+        <li>
+          <Button
+            className={styles["header-quiz-util-list-item"]}
+            icon={<img src={item.icon} width={24} height={24} alt="" />}
+            iconPosition="left"
+            onClick={item.onClick}
+          >
+            <span>{item.title}</span>
+          </Button>
+        </li>
+      ))}
     </ul>
   );
 }

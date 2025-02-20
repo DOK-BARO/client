868d3274a87e4df1cfadc00cf872d0e09bcbebf4
hyerimRoh <mtxpvksh@gmail.com>
2025-02-20 17:09:49 +0900

    Refactor(DK-582): 성능개선을 위해 manualChunks설정 - react, reactRouter, animation관련으로 나눠 설정

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { InlineConfig, UserConfig, defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import mkcert from "vite-plugin-mkcert";
 import { compression } from "vite-plugin-compression2";
+import { visualizer } from "rollup-plugin-visualizer";
 
 interface VitestConfigExport extends UserConfig {
   test: InlineConfig;
@@ -23,5 +24,23 @@ export default defineConfig({
     globals: true,
     environment: "jsdom",
     setupFiles: "./src/test/setup.ts",
+  },
+  build: {
+    rollupOptions: {
+      plugins: [
+        visualizer({
+          open: true,
+          gzipSize: true,
+          brotliSize: true,
+        }),
+      ],
+      output: {
+        manualChunks: {
+          react: ["react", "react-dom", "jotai", "@tanstack/react-query"],
+          reactRouter: ["react-router-dom"],
+          animations: ["lottie-react", "lottie-web"],
+        },
+      },
+    },
   },
 } as VitestConfigExport);

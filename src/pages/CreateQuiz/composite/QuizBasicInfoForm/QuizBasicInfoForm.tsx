53124575510d85026be0d795cbb079a79e0b34b6
page1597 <gimm08377@gmail.com>
2025-03-02 21:30:35 +0900

    Fix(DK-573): 변경된 값이 퀴즈 기본 정보에 바로 반영되지 않는 문제 수정

--- src/pages/CreateQuiz/composite/QuizBasicInfoForm/QuizBasicInfoForm.tsx
+++ src/pages/CreateQuiz/composite/QuizBasicInfoForm/QuizBasicInfoForm.tsx
@@ -2,33 +2,28 @@ import React from "react";
 import Input from "@/components/atom/Input/Input";
 import Textarea from "@/components/atom/Textarea/Textarea";
 import styles from "./_quiz_basic_info_form.module.scss";
-import useInput from "@/hooks/useInput.ts";
 import useAutoResizeTextarea from "@/hooks/useAutoResizeTextArea";
 import useUpdateQuizCreationInfo from "@/hooks/useUpdateQuizCreationInfo";
 
 function QuizBasicInfoForm() {
   const { quizCreationInfo, updateQuizCreationInfo } =
     useUpdateQuizCreationInfo();
+
   const titleMaxLength = 127;
   const descriptionMaxLength = 150;
-  const { value: titleInputValue, onChange: onTitleChange } = useInput(
-    quizCreationInfo.title ?? "",
+  const { textareaRef } = useAutoResizeTextarea(
+    quizCreationInfo.description ?? "",
+    56,
+    3,
   );
-  const {
-    value: descriptionTextareaValue,
-    onChange: onDescriptionChange,
-    textareaRef,
-  } = useAutoResizeTextarea(quizCreationInfo.description ?? "", 56, 3);
 
   const handleDescriptionChange = (
     e: React.ChangeEvent<HTMLTextAreaElement>,
   ) => {
-    onDescriptionChange(e);
     updateQuizCreationInfo("description", e.target.value);
   };
 
   const handleTitleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    onTitleChange(e);
     updateQuizCreationInfo("title", e.target.value);
   };
 
@@ -37,7 +32,7 @@ function QuizBasicInfoForm() {
       <Input
         size="large"
         id="quiz-basic-info-title"
-        value={titleInputValue}
+        value={quizCreationInfo.title ?? ""}
         onChange={handleTitleChange}
         placeholder="퀴즈 제목"
         maxLength={titleMaxLength}
@@ -48,7 +43,7 @@ function QuizBasicInfoForm() {
       <div className={styles["quiz-basic-info-description"]}>
         <Textarea
           id="quiz-basic-info-description"
-          value={descriptionTextareaValue}
+          value={quizCreationInfo.description ?? ""}
           onChange={handleDescriptionChange}
           placeholder="퀴즈 설명"
           maxLength={descriptionMaxLength}
